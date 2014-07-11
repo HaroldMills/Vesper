73923a9dd8904c0f9d40e2ccb05772b3583dbf6f
Harold Mills <harold.mills@gmail.com>
2014-07-11 09:44:41 -0400

    Add date parsing function to time utilities.

--- src/nfc/util/time_utils.py
+++ src/nfc/util/time_utils.py
@@ -2,12 +2,44 @@
 
 
 import calendar
+import datetime
+import re
 
 
+_DATE_RE = re.compile(r'^(\d\d\d\d)-(\d\d)-(\d\d)$')
 _MIN_YEAR = 1900
 _MAX_YEAR = 2099
 
 
+def parse_date(s):
+    
+    m = _DATE_RE.match(s)
+     
+    if m is None:
+        raise ValueError('Bad date "{:s}".'.format(s))
+     
+    else:
+         
+        year, month, day = m.groups()
+         
+        year = int(year)
+        month = int(month)
+        day = int(day)
+         
+        _check_field('year', s, check_year, year)
+        _check_field('month', s, check_month, month)
+        _check_field('day', s, check_day, day, year, month)
+             
+        return datetime.date(year, month, day)
+     
+ 
+def _check_field(name, s, function, *args):
+    try:
+        function(*args)
+    except ValueError:
+        raise ValueError('Bad {:s} in "{:s}".'.format(name, s))
+    
+    
 def check_year(year):
     # We do not reject all future years since we can think of legitimate
     # uses for some, for example in tables of DST start and end times.
