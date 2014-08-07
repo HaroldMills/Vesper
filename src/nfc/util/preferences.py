65e6f17c80584c519da9bbebeee3f3606fc26682
Harold Mills <harold.mills@gmail.com>
2014-08-07 11:03:07 -0400

    Move preferences JSON to its own file.

--- src/nfc/util/preferences.py
+++ src/nfc/util/preferences.py
@@ -2,122 +2,58 @@
 
 
 from __future__ import print_function
+
 import json
+import os.path
+import sys
         
             
-_COMMAND_SETS = {
-                 
-    'Coarse': '''
-        c Call
-        C Call Page
-        Alt-c Call All
-        n Noise
-        N Noise Page
-        Alt-n Noise All
-        t Tone
-        T Tone Page
-        Alt-t Tone All
-        u Unclassified
-        U Unclassified Page
-        Alt-u Unclassified All
-    ''',
-    
-    'Calls': '''
-        a AMRE
-        A ATSP
-        b BTBW
-        B BAWW
-        k Call
-        c CSWA
-        C CMWA
-        Ctrl-c COYE
-        Ctrl-C CCSP
-        Alt-c CAWA
-        Alt-C CHSP
-        D DbUp
-        f FOSP
-        g GHSP
-        h HOWA
-        H HESP
-        i INBU
-        l LCSP
-        m MOWA
-        n Noise
-        N NWTH
-        o OVEN
-        O Other
-        p PAWA
-        P PROW
-        q Unknown
-        s SVSP
-        S SwLi
-        Ctrl-s SNBULALO
-        t Tone
-        T ATSP
-        u Unclassified
-        v VESP
-        w WIWA
-        W WTSP
-        Alt-w WCSP
-        x Weak
-        y YRWA
-        z Zeep
-    '''
-    
-}
+_NFC_DIR_NAME = 'NFC'
+_PREFERENCES_FILE_NAME = 'Preferences.json'
 
 
 def _load_preferences():
     
+    home_dir_path = os.path.expanduser('~')
+    file_path = \
+        os.path.join(home_dir_path, _NFC_DIR_NAME, _PREFERENCES_FILE_NAME)
+    
     try:
-        preferences = json.loads(r'''
-{
-    "archiveDirPaths": {
-        "2012": "/Users/Harold/Desktop/NFC/Data/Old Bird/2012 Summer and Fall",
-        "2014": "/Users/Harold/Desktop/NFC/Data/Old Bird/2014"
-    },
-    "defaultArchive": "2012",
-    "stationName": "Alfred",
-    "detectorName": "Tseep",
-    "clipClassName": "Call",
-    "monthName": "September 2012",
-    "mainWindow.width": 1000,
-    "mainWindow.height": 800,
-    "mainWindow.countDisplayType": "archive calendar",
-    "clipsWindow.width": 1200,
-    "clipsWindow.height": 800,
-    "clipGrid.numRows": 4,
-    "clipGrid.numColumns": 5,
-    "clipGrid.showMouseTimeFreq": true,
-    "clipGrid.showClipClassNames": true,
-    "clipGrid.showClipTimes": true,
-    "clipGrid.clipTextFontSize": 11,
-    "clipGrid.clipTextColor": "white",
-    "clipGrid.clipClassTextColors": { "Call": "cyan" },
-    "classification.defaultCommandSet": "Coarse"
+        file_ = open(file_path, 'rU')
+    except:
+        f = 'Could not open preferences file "{:s}".'
+        _handle_error(f.format(file_path))
     
-}
-''')
-        
+    try:
+        preferences = json.load(file_)
     except ValueError:
-        print('Could not parse preferences file.')
-        return {}
-    
+        file_.close()
+        f = 'Could not parse preferences file "{:s}".'
+        _handle_error(f.format(file_path))
+
+    file_.close()
+        
     try:
         preferences['classification.commandSets'] = \
-            _parse_command_sets(_COMMAND_SETS)
+            _parse_command_sets(preferences['classification.commandSets'])
     except ValueError as e:
-        print(str(e))
-        
+        f = 'Could not parse command sets in preferences file "{:s}". {:s}'
+        _handle_error(f.format(file_path, str(e)))
+
     return preferences
 
 
+def _handle_error(message):
+    print(message, file=sys.stderr)
+    sys.exit(1)
+    
+    
 def _parse_command_sets(commandSets):
     return dict(_parse_command_set(*i) for i in commandSets.iteritems())
 
 
-def _parse_command_set(name, text):
-    lines = [line.strip() for line in text.splitlines()]
+def _parse_command_set(name, lines):
+    lines = [line.strip() for line in lines]
     pairs = [_parseCommand(line, i + 1, name)
              for i, line in enumerate(lines) if line != '']
     return (name, dict(pairs))
