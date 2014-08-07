c5ecdb4283874413a686dc4f0590ac4307761f78
Harold Mills <harold.mills@gmail.com>
2014-08-07 12:15:25 -0400

    Improve preferences support.
    Moved classification command set definitions from JSON preferences file
    to individual files in "Classification Command Sets" subdirectory. Added
    support for NFC_PREFS environment variable. Added example preferences
    file and classification command sets to project directory.

--- src/nfc/util/preferences.py
+++ src/nfc/util/preferences.py
@@ -4,43 +4,51 @@
 from __future__ import print_function
 
 import json
-import os.path
+import os
 import sys
         
             
-_NFC_DIR_NAME = 'NFC'
-_PREFERENCES_FILE_NAME = 'Preferences.json'
+_PREFS_DIR_NAME = 'NFC'
+_PREFS_VAR_NAME = 'NFC_PREFS'
+_PREFS_FILE_NAME = 'Preferences.json'
+_COMMAND_SETS_DIR_NAME = 'Classification Command Sets'
+_TEXT_FILE_NAME_EXTENSION = '.txt'
+_COMMAND_SCOPES = frozenset(['Selected', 'Page', 'All'])
 
 
 def _load_preferences():
     
-    home_dir_path = os.path.expanduser('~')
-    file_path = \
-        os.path.join(home_dir_path, _NFC_DIR_NAME, _PREFERENCES_FILE_NAME)
+    prefs_dir_path = _get_prefs_dir_path()
+    prefs_file_path = os.path.join(prefs_dir_path, _PREFS_FILE_NAME)
     
     try:
-        file_ = open(file_path, 'rU')
+        file_ = open(prefs_file_path, 'rU')
     except:
         f = 'Could not open preferences file "{:s}".'
-        _handle_error(f.format(file_path))
+        _handle_error(f.format(prefs_file_path))
     
     try:
-        preferences = json.load(file_)
+        prefs = json.load(file_)
     except ValueError:
         file_.close()
         f = 'Could not parse preferences file "{:s}".'
-        _handle_error(f.format(file_path))
+        _handle_error(f.format(prefs_file_path))
 
     file_.close()
         
     try:
-        preferences['classification.commandSets'] = \
-            _parse_command_sets(preferences['classification.commandSets'])
+        prefs['classification.commandSets'] = \
+            _parse_command_sets(prefs_dir_path)
     except ValueError as e:
-        f = 'Could not parse command sets in preferences file "{:s}". {:s}'
-        _handle_error(f.format(file_path, str(e)))
+        _handle_error(str(e))
 
-    return preferences
+    return prefs
+
+
+def _get_prefs_dir_path():
+    home_dir_path = os.path.expanduser('~')
+    default_prefs_dir_path = os.path.join(home_dir_path, _PREFS_DIR_NAME)
+    return os.environ.get(_PREFS_VAR_NAME, default_prefs_dir_path)
 
 
 def _handle_error(message):
@@ -48,39 +56,84 @@ def _handle_error(message):
     sys.exit(1)
     
     
-def _parse_command_sets(commandSets):
-    return dict(_parse_command_set(*i) for i in commandSets.iteritems())
+def _parse_command_sets(prefs_dir_path):
+    
+    command_sets = {}
+    dir_path = os.path.join(prefs_dir_path, _COMMAND_SETS_DIR_NAME)
+    
+    for _, _, file_names in os.walk(dir_path):
+        
+        for file_name in file_names:
+            
+            name = _get_command_set_name(file_name)
+            file_path = os.path.join(dir_path, file_name)
+            
+            try:
+                command_sets[name] = _parse_command_set(file_path)
+                
+            except ValueError as e:
+                print(str(e))
+                continue
+            
+    return command_sets
+            
+            
+def _get_command_set_name(file_name):
+    if file_name.endswith(_TEXT_FILE_NAME_EXTENSION):
+        return file_name[:-len(_TEXT_FILE_NAME_EXTENSION)]
+    else:
+        return file_name
+    
+    
+def _parse_command_set(file_path):
+    
+    try:
+        file_ = open(file_path, 'rU')
+    except:
+        f = 'Could not open classification command set file "{:s}".'
+        raise ValueError(f.format(file_path))
+    
+    try:
+        text = file_.read()
+    except ValueError:
+        f = 'Could not read classification command set file "{:s}".'
+        raise ValueError(f.format(file_path))
+    finally:
+        file_.close()
+        
+    try:
+        return _parse_command_set_aux(text)
+    except ValueError as e:
+        f = 'Could not parse classification command set file "{:s}". {:s}'
+        raise ValueError(f.format(file_path, str(e)))
+    
+    
+def _parse_command_set_aux(text):
+    lines = [line.strip() for line in text.split('\n')]
+    lines = [line for line in lines if line != '']
+    pairs = [_parseCommand(line, i + 1) for i, line in enumerate(lines)]
+    return dict(pairs)
 
 
-def _parse_command_set(name, lines):
-    lines = [line.strip() for line in lines]
-    pairs = [_parseCommand(line, i + 1, name)
-             for i, line in enumerate(lines) if line != '']
-    return (name, dict(pairs))
-
-
-_SCOPES = frozenset(['Selected', 'Page', 'All'])
-
-
-def _parseCommand(line, lineNum, commandSetName):
+def _parseCommand(line, lineNum):
     
     items = line.split()
     n = len(items)
     
     if n < 2 or n > 3:
         raise ValueError(
-            ('Bad command specification "{:s}" for command set "{:s}": '
-             'specification must have either two or three components '
-             'separated by spaces.').format(line, commandSetName))
+            ('Bad command specification "{:s}": specification '
+             'must have either two or three components separated '
+             'by spaces.').format(line))
     
     name = items[0]
     command = items[1]
     scope = items[2] if n == 3 else 'Selected'
     
-    if scope not in _SCOPES:
-        f = ('Bad command specification "{:s}" for command set "{:s}": '
-             'third component must be "Page" or "All".')
-        raise ValueError(f.format(line, commandSetName))
+    if scope not in _COMMAND_SCOPES:
+        f = ('Bad command specification "{:s}": third component '
+             'must be "Page" or "All".')
+        raise ValueError(f.format(line))
         
     return (name, (command, scope))
 
