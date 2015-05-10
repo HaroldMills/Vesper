74c72f07f5347ac0b5436b707835c9eca9ff96f0
HaroldMills <harold.mills@gmail.com>
2015-05-10 11:05:35 -0400

    Added text utilities module.

---
+++ src/vesper/util/text_utils.py
@@ -0,0 +1,31 @@
+"""Utility functions pertaining to text."""
+
+
+def format_number(x):
+    
+    # Formats a nonnegative number either as an integer if it rounds to
+    # at least ten, or as a decimal rounded to two significant digits.
+    
+    if x == 0:
+        return '0'
+    
+    else:
+        
+        i = int(round(x))
+        
+        if i < 10:
+            
+            # Get `d` as decimal rounded to two significant digits.
+            
+            xx = x
+            n = 0
+            while xx < 10:
+                xx *= 10
+                n += 1
+            f = '{:.' + str(n) + 'f}'
+            return f.format(x)
+    
+        else:
+            return str(i)
+    
+    
