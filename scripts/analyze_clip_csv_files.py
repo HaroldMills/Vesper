393c79f9e8a3fa1303c65000a94407d1009f2638
HaroldMills <harold.mills@gmail.com>
2015-11-23 12:27:06 -0500

    Added Thrush noise support to clip CSV file analyzer.

--- scripts/analyze_clip_csv_files.py
+++ scripts/analyze_clip_csv_files.py
@@ -11,24 +11,21 @@ import pandas as pd
 
 
 _CSV_DIR_PATH = r'C:\Users\Harold\Desktop\NFC\Data\MPG Ranch'
-_CALLS_FILE_PATH = os.path.join(_CSV_DIR_PATH, 'Call Clips.csv')
-_NOISES_FILE_PATH = os.path.join(_CSV_DIR_PATH, 'Tseep Noise Clips.csv')
+_CALLS_FILE_NAME = 'Call Clips.csv'
+_NOISES_FILE_NAME_SUFFIX = ' Noise Clips.csv'
 _STATION_NAMES = ['Floodplain NFC', 'Sheep Camp NFC', 'Ridge NFC']
+_DETECTOR_NAMES = ['Tseep', 'Thrush']
 
 
 def _main():
     
     all_clips = _read_csv_files()
     
-    # Exclude rows not from stations of interest.
-    mask = all_clips['Station'].isin(_STATION_NAMES)
-    all_clips = all_clips[mask]
-    
     # Add coarse clip class column.
     all_clips['Coarse Clip Class'] = \
         all_clips['Clip Class'].apply(_get_first_clip_class_component)
         
-    for detector_name in ['Tseep', 'Thrush']:
+    for detector_name in _DETECTOR_NAMES:
         
         mask = all_clips['Detector'] == detector_name
         clips = all_clips[mask]
@@ -53,11 +50,39 @@ def _main():
         
 
 def _read_csv_files():
-    calls = pd.read_csv(_CALLS_FILE_PATH)
-    noises = pd.read_csv(_NOISES_FILE_PATH)
-    return calls.append(noises)
+    df = _read_clips_csv_file()
+    for detector_name in _DETECTOR_NAMES:
+        df = df.append(_read_noises_csv_file(detector_name))
+    return df
 
 
+def _read_clips_csv_file():
+    
+    df = _read_csv_file(_CALLS_FILE_NAME)
+    
+    # Exclude noise clips.
+    mask = df['Clip Class'] != 'Noise'
+    df = df[mask]
+    
+    return df
+
+
+def _read_csv_file(file_name):
+    
+    file_path = os.path.join(_CSV_DIR_PATH, file_name)
+    df = pd.read_csv(file_path)
+    
+    # Exclude rows not for stations of interest
+    mask = df['Station'].isin(_STATION_NAMES)
+    df = df[mask]
+    
+    return df
+
+
+def _read_noises_csv_file(detector_name):
+    return _read_csv_file(detector_name + _NOISES_FILE_NAME_SUFFIX)
+    
+    
 def _get_first_clip_class_component(clip_class):
     
     if clip_class.startswith('Call'):
