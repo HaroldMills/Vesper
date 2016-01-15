c2d26f54f2affa3ff2ca9b1086aa820abfbb18ed
HaroldMills <harold.mills@gmail.com>
2016-01-14 21:32:33 -0500

    Tweaked a signal utilities function argument name.

--- src/vesper/util/signal_utils.py
+++ src/vesper/util/signal_utils.py
@@ -1,5 +1,5 @@
 """Utility functions pertaining to signals."""
 
 
-def seconds_to_frames(duration, frame_rate):
-    return int(round(duration * frame_rate))
+def seconds_to_frames(seconds, frame_rate):
+    return int(round(seconds * frame_rate))
