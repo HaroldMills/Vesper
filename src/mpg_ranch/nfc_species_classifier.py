HaroldMills/Vesper

commit b9c4dbe4a17e31ed9f791eb720048b38a1e4cd8d

Added viewer support for automatic classification commands.
Implemented commands for MPG Ranch "Outside" classification, NFC coarse
classification, and MPG Ranch species classification.

src/mpg_ranch/nfc_species_classifier.py
@@ -68,7 +68,7 @@ class _ClipVisitor(ClipVisitor):
     
     def __init__(self, positional_args, keyword_args):
         super(_ClipVisitor, self).__init__(positional_args, keyword_args)
-        self._classifiers = _create_classifiers()
+        self._classifier = NfcSpeciesClipClassifier()
         
         
     def visit(self, clip):
@@ -80,8 +80,27 @@ class _ClipVisitor(ClipVisitor):
                     clip.clip_class_name = clip_class_name
                 
 
-def _create_classifiers():
-    return dict((name, _create_classifier(name)) for name in _DETECTOR_NAMES)
+class NfcSpeciesClipClassifier(object):
+    
+    
+    name = 'MPG Ranch NFC Species Clip Classifier'
+    
+    
+    def __init__(self):
+        self._classifiers = {}
+        for name in _DETECTOR_NAMES:
+            classifier = _create_classifier(name)
+            if classifier is not None:
+                self._classifiers[name] = classifier
+        
+        
+    def classify(self, clip):
+        if clip.clip_class_name == 'Call':
+            classifier = self._classifiers.get(clip.detector_name)
+            if classifier is not None:
+                clip_class_name = classifier.classify_clip(clip)
+                if clip_class_name is not None:
+                    clip.clip_class_name = clip_class_name
 
 
 def _create_classifier(name):
@@ -90,6 +109,10 @@ def _create_classifier(name):
     file_name = '{} Species Classifier.pkl'.format(name)
     file_path = os.path.join(package_dir_path, file_name)
 
-    # TODO: Handle load errors.
-    with open(file_path, 'r') as file_:
-        return pickle.load(file_)
+    try:
+        with open(file_path, 'r') as file_:
+            return pickle.load(file_)
+    except Exception as e:
+        raise ValueError(
+            ('Could not create classifier "{}". Error message '
+             'was: {}').format(name, str(e)))

src/mpg_ranch/outside_classifier.py
@@ -74,38 +74,38 @@ class OutsideClassifier(object):
 class _ClipVisitor(ClipVisitor):
     
     
+    def __init__(self, positional_args, keyword_args):
+        super(_ClipVisitor, self).__init__(positional_args, keyword_args)
+        self._classifier = OutsideClipClassifier()
+        
+        
     def visit(self, clip):
-        if _is_clip_outside_monitoring_period(clip):
-            clip.clip_class_name = 'Outside'
+        self._classifier.classify(clip)
             
 
-def _is_clip_outside_monitoring_period(clip):
+class OutsideClipClassifier(object):
     
-    timedelta = datetime.timedelta
     
-    station = clip.station
+    name = 'MPG Ranch Outside Clip Classifier'
     
-    if station.latitude is None or station.longitude is None:
-        # station location unknown
-        
-        return False
     
-    else:
-        # station location known
-        
-        sunset_time = station.get_sunset_time(clip.night)
-        start_time = sunset_time + timedelta(minutes=_START_OFFSET)
-        
-        sunrise_date = clip.night + timedelta(days=1)
-        sunrise_time = station.get_sunrise_time(sunrise_date)
-        end_time = sunrise_time + timedelta(minutes=_END_OFFSET)
-        
-        time = clip.start_time
-        outside = time < start_time or time > end_time
+    def classify(self, clip):
 
-#         print(clip.start_time,
-#               'sunset', sunset_time, 'start', start_time,
-#               'sunrise', sunrise_time, 'end', end_time,
-#               'outside', outside)
-
-        return outside
+        station = clip.station
+        
+        if station.latitude is not None and station.longitude is not None:
+            # station location known
+            
+            timedelta = datetime.timedelta
+        
+            sunset_time = station.get_sunset_time(clip.night)
+            start_time = sunset_time + timedelta(minutes=_START_OFFSET)
+            
+            sunrise_date = clip.night + timedelta(days=1)
+            sunrise_time = station.get_sunrise_time(sunrise_date)
+            end_time = sunrise_time + timedelta(minutes=_END_OFFSET)
+            
+            time = clip.start_time
+            
+            if time < start_time or time > end_time:
+                clip.clip_class_name = 'Outside'

src/vesper/ui/clips_window.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import math
+import operator
 
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
@@ -17,7 +18,6 @@ from vesper.ui.spectrogram_clip_figure import \
     SpectrogramClipFigure as ClipFigure
 from vesper.util.bunch import Bunch
 from vesper.util.preset_manager import preset_manager
-import vesper.util.classification_command_utils as command_utils
 import vesper.util.preferences as prefs
 
 
@@ -243,7 +243,7 @@ class ClipsWindow(QMainWindow):
         
     def keyPressEvent(self, e):
         
-        command_name = command_utils.get_command_from_key_event(e)
+        command_name = _get_command_from_key_event(e)
         
         if command_name is not None:
 
@@ -267,7 +267,7 @@ class ClipsWindow(QMainWindow):
             
     def _key_press_event(self, e):
         
-        is_key = command_utils.is_key
+        is_key = _is_key
         
         if is_key(e, Qt.Key_Space) or is_key(e, Qt.Key_PageDown):
             self.move_down_one_page()
@@ -313,6 +313,50 @@ class ClipsWindow(QMainWindow):
     def move_singleton_selection_backward(self):
         self._figures_frame.move_singleton_selection_backward()
         self._update_title()
+
+
+_MODIFIER_PAIRS = [('Alt', Qt.AltModifier)]
+"""
+list of recognized (modifier name, QT keyboard modifier flag) pairs,
+excluding shift.
+
+Note that we do *not* allow classification commands that use the control
+modifier (i.e. the control key on Linux and Windows and the command key
+on Mac OS X) since they could collide with menu item keyboard accelerators.
+"""
+
+_ALL_MODIFIERS = reduce(
+    operator.or_, [m for _, m in _MODIFIER_PAIRS], Qt.ShiftModifier)
+"""disjunction of recognized command modifiers, including shift."""
+
+
+def _get_command_from_key_event(key_event):
+    
+    char = str(key_event.text())
+    
+    if char == '':
+        return None
+    
+    else:
+        
+        modifiers = key_event.modifiers()
+        
+        if modifiers | _ALL_MODIFIERS != _ALL_MODIFIERS:
+            # unrecognized modifier present
+            return None
+            
+        mods = ''.join(s + '-' for s, m in _MODIFIER_PAIRS if modifiers & m)
+        
+        return mods + char
+    
+    
+def _is_key(key_event, key, modifiers=Qt.NoModifier):
+     
+    if key_event.key() != key:
+        return False
+     
+    else:
+        return key_event.modifiers() == modifiers
 
 
 class _FiguresFrame(QWidget):

src/vesper/util/classification_command_utils.py
@@ -1,68 +1,0 @@
-"""Utility functions pertaining to classification command sets."""
-
-
-import operator
-
-from PyQt4.QtCore import Qt
-
-
-_ALPHABETIC_CHARS = 'abcdefghijklmnopqrstuvwxyz'
-"""string of lower-case alphabetic characters."""
-
-_CHARS_DICT = dict((eval('Qt.Key_' + c.upper()), c) for c in _ALPHABETIC_CHARS)
-"""mapping from integer Qt key codes to lower-case characters."""
-
-_CHARS = frozenset(_ALPHABETIC_CHARS + _ALPHABETIC_CHARS.upper())
-"""set of recognized command characters."""
-
-_MODIFIER_PAIRS = [('Alt', Qt.AltModifier)]
-"""
-list of recognized (modifier name, QT keyboard modifier flag) pairs,
-excluding shift.
-
-Note that we do *not* allow classification commands that use the control
-modifier (i.e. the control key on Linux and Windows and the command key
-on Mac OS X) since they could collide with menu item keyboard accelerators.
-"""
-
-_MODIFIER_NAMES = frozenset([p[0] for p in _MODIFIER_PAIRS])
-"""set of recognized command modifier names, excluding shift."""
-
-_ALL_MODIFIERS = reduce(
-    operator.or_, [m for _, m in _MODIFIER_PAIRS], Qt.ShiftModifier)
-"""disjunction of recognized command modifiers, including shift."""
-
-_SCOPES = frozenset(['Selected', 'Page', 'All'])
-"""set of recognized command scopes."""
-
-
-def get_command_from_key_event(key_event):
-    
-    char = _CHARS_DICT.get(key_event.key())
-    
-    if char is None:
-        return None
-    
-    else:
-        
-        modifiers = key_event.modifiers()
-        
-        if modifiers | _ALL_MODIFIERS != _ALL_MODIFIERS:
-            # unrecognized modifier present
-            return None
-            
-        mods = ''.join(s + '-' for s, m in _MODIFIER_PAIRS if modifiers & m)
-
-        if modifiers & Qt.ShiftModifier:
-            char = char.upper()
-            
-        return mods + char
-
-
-def is_key(key_event, key, modifiers=Qt.NoModifier):
-     
-    if key_event.key() != key:
-        return False
-     
-    else:
-        return key_event.modifiers() == modifiers

src/vesper/util/classification_commands_preset.py
@@ -5,35 +5,24 @@ import six
 import yaml
 
 from vesper.util.preset import Preset
+import vesper.util.extension_manager as extension_manager
 
 
 class ClassificationCommandsPreset(Preset):
     
     """Preset for a set of clip classification commands."""
     
-    
     type_name = 'Classification Commands'
-    
     
     def __init__(self, name, data):
         super(ClassificationCommandsPreset, self).__init__(name)
         self._commands = _parse_preset(data)
 
-    
     @property
     def commands(self):
         return dict(self._commands)
         
         
-_ALPHABETIC_CHARS = 'abcdefghijklmnopqrstuvwxyz'
-"""string of lower-case alphabetic characters."""
-
-_OTHER_CHARS = ',.;:\'"/?<>'
-
-_CHARS = frozenset(
-    _ALPHABETIC_CHARS + _ALPHABETIC_CHARS.upper() + _OTHER_CHARS)
-"""set of recognized command characters."""
-
 _MODIFIERS = frozenset(['Alt'])
 """set of recognized command modifiers, excluding shift."""
 
@@ -95,14 +84,8 @@ def _check_command_name(name):
         
         
 def _check_command_char(char):
-    
     if len(char) != 1:
         raise ValueError('A command must have exactly one character.')
-    
-    if char not in _CHARS:
-        raise ValueError(
-            ('Only alphabetic characters and characters in '
-             '{} can be command characters.'.format(_OTHER_CHARS)))
 
 
 def _check_modifiers(modifiers):
@@ -165,9 +148,18 @@ def _create_classify_action(spec):
         return _ClassifyAction(classifier)
     
     elif spec.has_key('classifier'):
-        # classifier_name = spec['classifier']
-        # TODO: Get classifier from extensions manager.
-        classifier = _DummyClassifier()
+        
+        name = spec['classifier']
+        classes = extension_manager.get_extensions('Clip Classifier')
+        
+        try:
+            cls = classes[name]
+        except KeyError:
+            raise ValueError(
+                'Unrecognized clip classifier name "{}".'.format(name))
+            
+        classifier = cls()
+        
         return _ClassifyAction(classifier)
     
     else:
@@ -191,24 +183,13 @@ class _FixedClassifier(object):
     
 class _ClassifyAction(object):
     
-    
     def __init__(self, classifier):
         self._classifier = classifier
         
-        
     def execute(self, clip):
-        
         clip_class_name = self._classifier.classify(clip)
-        
         if clip_class_name is not None:
-            
             if clip_class_name == 'Unclassified':
                 clip_class_name = None
-                
             clip.clip_class_name = clip_class_name
-
-
-class _DummyClassifier(object):
-    def classify(self, clip):
-        return None
     

src/vesper/util/extension_manager.py
@@ -38,7 +38,12 @@ def load_extensions():
     from mpg_ranch.nfc_importer import NfcImporter as MpgRanchNfcImporter
     from mpg_ranch.outside_classifier \
         import OutsideClassifier as MpgRanchOutsideClassifier
-    from mpg_ranch.nfc_species_classifier import NfcSpeciesClassifier
+    from mpg_ranch.outside_classifier \
+        import OutsideClipClassifier as MpgRanchOutsideClipClassifier
+    from mpg_ranch.nfc_species_classifier \
+        import NfcSpeciesClassifier as MpgRanchNfcSpeciesClassifier
+    from mpg_ranch.nfc_species_classifier \
+        import NfcSpeciesClipClassifier as MpgRanchNfcSpeciesClipClassifier
     from old_bird.detector import Detector as OldBirdDetector
     from vesper.vcl.classify_command import ClassifyCommand
     from vesper.vcl.clips_exporter import ClipsExporter
@@ -48,6 +53,7 @@ def load_extensions():
     from vesper.vcl.help_command import HelpCommand
     from vesper.vcl.import_command import ImportCommand
     from vesper.vcl.nfc_coarse_classifier import NfcCoarseClassifier
+    from vesper.vcl.nfc_coarse_classifier import NfcCoarseClipClassifier
     from vesper.vcl.sample_command import SampleCommand
 
     global _extensions
@@ -57,9 +63,15 @@ def load_extensions():
         'VCL Classifier': (
             MpgRanchOutsideClassifier,
             NfcCoarseClassifier,
-            NfcSpeciesClassifier,
+            MpgRanchNfcSpeciesClassifier,
         ),
                    
+        'Clip Classifier': (
+            MpgRanchOutsideClipClassifier,
+            NfcCoarseClipClassifier,
+            MpgRanchNfcSpeciesClipClassifier,
+        ),
+         
         'VCL Command': (
             ClassifyCommand,
             CreateCommand,

src/vesper/vcl/nfc_coarse_classifier.py
@@ -62,18 +62,40 @@ class _ClipVisitor(ClipVisitor):
     
     def __init__(self, positional_args, keyword_args):
         super(_ClipVisitor, self).__init__(positional_args, keyword_args)
-        self._classifiers = _create_classifiers()
+        self._classifier = NfcCoarseClipClassifier()
         
         
     def visit(self, clip):
+        self._classifier.classify(clip)
+                
+
+class NfcCoarseClipClassifier(object):
+    
+    
+    name = 'NFC Coarse Clip Classifier'
+    
+    
+    def __init__(self):
+        self._classifiers = {}
+        for name in _DETECTOR_NAMES:
+            classifier = _create_classifier(name)
+            if classifier is not None:
+                self._classifiers[name] = classifier
+        
+        
+    def classify(self, clip):
         if clip.clip_class_name is None:
             classifier = self._classifiers.get(clip.detector_name)
             if classifier is not None:
                 clip_class_name = classifier.classify_clip(clip)
                 if clip_class_name is not None:
                     clip.clip_class_name = clip_class_name
-                
 
-def _create_classifiers():
-    create_classifier = nfc_coarse_classifier.create_classifier
-    return dict((name, create_classifier(name)) for name in _DETECTOR_NAMES)
+
+def _create_classifier(name):
+    try:
+        return nfc_coarse_classifier.create_classifier(name)
+    except Exception as e:
+        raise ValueError(
+            ('Could not create classifier "{}". Error message '
+             'was: {}').format(name, str(e)))

commit 49dcb3d3ff9f08fa3bce46a801dd08b467543b2b

Fixed species classifier bug introduced last week.
The bug caused species classification via VCL to fail with an
`AttributeError`. The bug was introduced in
b9c4dbe4a17e31ed9f791eb720048b38a1e4cd8d.

src/mpg_ranch/nfc_species_classifier.py
@@ -71,12 +71,7 @@ class _ClipVisitor(ClipVisitor):
         
         
     def visit(self, clip):
-        if clip.clip_class_name == 'Call':
-            classifier = self._classifiers.get(clip.detector_name)
-            if classifier is not None:
-                clip_class_name = classifier.classify_clip(clip)
-                if clip_class_name is not None:
-                    clip.clip_class_name = clip_class_name
+        self._classifier.classify(clip)
                 
 
 class NfcSpeciesClipClassifier(object):