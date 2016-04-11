b636a365cc3705a3b75f951536ff2f803329c45a
HaroldMills <harold.mills@gmail.com>
2016-04-11 16:31:07 -0400

    Added conda packaging files.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='vesper',
+    version='0.0.1',
+    description='Software for bioacoustical sound analysis.',
+    url='https://github.com/HaroldMills/Vesper',
+    author='Harold Mills',
+    author_email='harold.mills@gmail.com',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'matplotlib',
+        'pandas',
+        'pyephem',
+        'pyyaml',
+        'scikit-learn'
+    ],
+    entry_points={
+        'console_scripts': [
+            'vcl=vesper.vcl.vcl:main',
+            'vesper_viewer=vesper.ui.vesper_viewer:main'
+        ]
+    },
+    include_package_data=True,
+    zip_safe=False
+)
