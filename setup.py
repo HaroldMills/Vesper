729885768deef6bb28af7140b24b12b5b301ff66
HaroldMills <harold.mills@gmail.com>
2016-04-12 16:20:12 -0400

    Excluded unit test packages in setup.py.
    Remembered that some of the unit test packages contain large amounts
    (tens of megabytes) of data, such as audio files.

--- setup.py
+++ setup.py
@@ -12,14 +12,10 @@ setup(
     author_email='harold.mills@gmail.com',
     license='MIT',
     
-    packages=find_packages(),
-    
-    # I decided to include all Python packages in the distribution,
-    # including the unit tests, since that is most straightforward.
-    # Prior to making that decision I tested the following for
-    # excluding all packages named "tests", and it worked.
-    # packages=find_packages(
-    #     exclude=['tests', 'tests.*', '*.tests.*', '*.tests']),
+    # We exclude the unit test packages since some of them contain a
+    # lot of data, for example large audio files.
+    packages=find_packages(
+        exclude=['tests', 'tests.*', '*.tests.*', '*.tests']),
     
     install_requires=[
         'matplotlib',
