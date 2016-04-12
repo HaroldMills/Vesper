1e2044efb22886bac9be876f997e9aaf2ccb880c
HaroldMills <harold.mills@gmail.com>
2016-04-12 15:47:17 -0400

    Updated setup.py to not exclude unit tests.

--- setup.py
+++ setup.py
@@ -2,15 +2,25 @@ from setuptools import find_packages, setup
 
 
 setup(
+      
     name='vesper',
     version='0.0.1',
-    description='Software for acoustical nocturnal bird migration monitoring.',
+    description=\
+        'Software for acoustical monitoring of nocturnal bird migration.',
     url='https://github.com/HaroldMills/Vesper',
     author='Harold Mills',
     author_email='harold.mills@gmail.com',
     license='MIT',
-    packages=find_packages(
-        exclude=['tests', 'tests.*', '*.tests.*', '*.tests']),
+    
+    packages=find_packages(),
+    
+    # I decided to include all Python packages in the distribution,
+    # including the unit tests, since that is most straightforward.
+    # Prior to making that decision I tested the following for
+    # excluding all packages named "tests", and it worked.
+    # packages=find_packages(
+    #     exclude=['tests', 'tests.*', '*.tests.*', '*.tests']),
+    
     install_requires=[
         'matplotlib',
         'pandas',
@@ -18,12 +28,15 @@ setup(
         'pyyaml',
         'scikit-learn'
     ],
+      
     entry_points={
         'console_scripts': [
             'vcl=vesper.vcl.vcl:main',
             'vesper_viewer=vesper.ui.vesper_viewer:main'
         ]
     },
+      
     include_package_data=True,
     zip_safe=False
+    
 )
