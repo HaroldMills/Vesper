4baff362751e3aa68e4b39c8d537274dcdc91376
HaroldMills <harold.mills@gmail.com>
2016-09-14 17:02:34 -0400

    Added number formatting function to text utilities.
    The function will be used for Old Bird detector logging.

--- vesper/util/tests/test_text_utils.py
+++ vesper/util/tests/test_text_utils.py
@@ -20,3 +20,35 @@ class TextUtilsTests(TestCase):
             result = text_utils.create_string_item_list(items)
             self.assertEqual(result, expected)
         
+        
+    def test_format_number(self):
+        
+        cases = [
+                 
+            # integers
+            (0, '0'),
+            (1, '1.0'),
+            (9, '9.0'),
+            (10, '10'),
+            (99, '99'),
+            (1234, '1234'),
+            
+            # floats
+            (.0000000001, '0.00000000010'),
+            (.00000000011, '0.00000000011'),
+            (.123, '0.12'),
+            (.125, '0.12'),
+            (.12500001, '0.13'),
+            (1.23, '1.2'),
+            (1.25, '1.2'),
+            (1.2500001, '1.3'),
+            (9.49999, '9.5'),
+            (9.5, '10'),
+            (10.1, '10'),
+            (1234.56, '1235')
+            
+        ]
+        
+        for x, expected in cases:
+            result = text_utils.format_number(x)
+            self.assertEqual(result, expected)
--- vesper/util/text_utils.py
+++ vesper/util/text_utils.py
@@ -1,3 +1,6 @@
+"""Utility functions pertaining to text."""
+
+
 def create_string_item_list(items):
     
     """
@@ -29,3 +32,33 @@ def create_string_item_list(items):
     else:
         final_item = 'and ' + items[-1]
         return ', '.join(items[:-1] + [final_item])
+
+
+def format_number(x):
+    
+    """
+    Formats a nonnegative number either as an integer if it rounds to
+    at least ten, or as a decimal rounded to two significant digits.
+    """
+    
+    if x == 0:
+        return '0'
+    
+    else:
+        
+        i = int(round(x))
+        
+        if i < 10:
+            
+            # Get `d` as decimal rounded to two significant digits.
+            
+            xx = x
+            n = 0
+            while xx < 10:
+                xx *= 10
+                n += 1
+            f = '{:.' + str(n) + 'f}'
+            return f.format(x)
+    
+        else:
+            return str(i)
