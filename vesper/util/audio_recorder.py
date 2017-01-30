7c5d59ce0e5c509f945be123c7b03b165f358f6f
HaroldMills <harold.mills@gmail.com>
2017-01-30 08:15:10 -0500

    Added recording thread to audio recorder class.
    All PyAudio control and input listener notification happens on the
    recording thread. The PyAudio callback thread now just queues input
    buffers for the recording thread to handle rather than notifying
    listeners itself.

--- vesper/util/audio_recorder.py
+++ vesper/util/audio_recorder.py
@@ -1,12 +1,43 @@
 """Module containing the `AudioRecorder` class."""
 
 
-from threading import Lock
+from queue import Queue
+from threading import Thread
 
 import pyaudio
 
+from vesper.util.bunch import Bunch
 from vesper.util.notifier import Notifier
 from vesper.util.schedule import ScheduleRunner
+
+
+# TODO:
+#
+# 1. Give each `AudioRecorder` its own thread that reads audio data from
+#    a `queue`. The audio data are written to the `queue` by the recorder's
+#    PyAudio callback. This will keep the PyAudio callback execution time
+#    to a minimum by moving `AudioRecorderListener` processing from the
+#    callback thread to the recorder thread.
+#
+# 2. Review the schedule listener methods and the `AudioRecorder.wait`
+#    method. Are they what we want? How will scheduled recording relate
+#    to scheduled processing of recordings, e.g. detection followed by
+#    coarse classification?
+#
+# 3. Implement a recorder command that gets a list of available input
+#    devices.
+#
+# 4. Make the `AudioRecorder` configuration mutable. Some state will
+#    be mutable only when a recorder is stopped and its schedule is
+#    disabled. As before, synchronize state manipulation with a lock
+#    where needed.
+#
+# 5. Add HTTP access to `AudioRecorder` state. Include a way to get
+#    a list of available input devices.
+#
+# 6. Test local deployment as a Windows service.
+#
+# 7. Test MPG Ranch deployment as a Windows service.
 
 
 class AudioRecorder:
@@ -28,9 +59,14 @@ class AudioRecorder:
         self._schedule = schedule
         
         self._recording = False
-        self._notifier = Notifier()
-        self._lock = Lock()
+        self._stop_pending = False
         
+        self._notifier = Notifier()
+        
+        self._command_queue = Queue()
+        self._thread = Thread(target=self._run)
+        self._thread.start()
+
         if schedule is not None:
             self._schedule_runner = ScheduleRunner(schedule)
             listener = _ScheduleListener(self)
@@ -69,7 +105,7 @@ class AudioRecorder:
     
     
     def remove_listener(self, listener):
-        self._notiier.remove_listener(listener)
+        self._notifier.remove_listener(listener)
     
     
     def clear_listeners(self):
@@ -81,24 +117,51 @@ class AudioRecorder:
             
             
     def start(self):
-        
-        with self._lock:
-            
-            if self._schedule_runner is not None:
-                self._schedule_runner.start()
-                
-            else:
-                self._start()
+        if self._schedule_runner is not None:
+            self._schedule_runner.start()
+        else:
+            self._start()
                 
                 
     def _start(self):
+        command = Bunch(name='start')
+        self._command_queue.put(command)
+            
+    
+    def stop(self):
+        if self._schedule_runner is not None:
+            self._schedule_runner.stop()
+        else:
+            self._stop()
+                
+                
+    def _stop(self):
+        command = Bunch(name='stop')
+        self._command_queue.put(command)
+            
+            
+    def _run(self):
+        
+        # TODO: What do we do if this method raises an exception?
+        
+        while True:
+            
+            command = self._command_queue.get()
+
+            handler_name = '_on_' + command.name
+            handler = getattr(self, handler_name)
+            handler(command)
+                
+    
+    def _on_start(self, command):
         
         if not self._recording:
             
             self._notify_listeners('recording_starting')
-                
-            self._recording = True
             
+            self._recording = True
+            self._stop_pending = False
+
             self._pyaudio = pyaudio.PyAudio()
             
             self._stream = self._pyaudio.open(
@@ -107,45 +170,57 @@ class AudioRecorder:
                 rate=self.sample_rate,
                 frames_per_buffer=self.buffer_size,
                 input=True,
-                stream_callback=self._callback)
+                stream_callback=self._pyaudio_callback)
     
-    
-    def _callback(self, samples, buffer_size, time_info, status):
-        
-        with self._lock:
-        
-            if self._recording:
-                self._notify_listeners('samples_arrived', samples, buffer_size)
-                return (None, pyaudio.paContinue)
-            
-            else:
-                return (None, pyaudio.paComplete)
 
-
-    def stop(self):
+    def _pyaudio_callback(self, samples, buffer_size, time_info, status):
         
-        with self._lock:
-            
-            if self._schedule_runner is not None:
-                self._schedule_runner.stop()
-                
-            else:
-                self._stop()
-                
-                
-    def _stop(self):
-                
+        # TODO: Should we copy samples before queueing them?
+        
         if self._recording:
             
+            command = Bunch(
+                name='input',
+                samples=samples,
+                buffer_size=buffer_size)
+            
+            self._command_queue.put(command)
+            
+            return (None, pyaudio.paContinue)
+        
+        else:
+            return (None, pyaudio.paComplete)
+
+
+    def _on_input(self, command):
+        
+        self._notify_listeners(
+            'samples_arrived', command.samples, command.buffer_size)
+
+        if self._stop_pending:
+            
             self._recording = False
+            self._stop_pending = False
             
             self._stream.stop_stream()
             self._stream.close()
             self._pyaudio.terminate()
             
             self._notify_listeners('recording_stopped')
-            
-            
+
+
+    def _on_stop(self, command):
+        
+        # Instead of stopping recording here, we just set a flag
+        # indicating that a stop is pending, and then stop in the
+        # next call to the`_on_input` method *after* processing
+        # the next buffer of input samples. If we stop here we
+        # usually record one less buffer than one would expect.
+        if self._recording:
+            self._stop_pending = True
+    
+
+    # TODO: Review the schedule listener methods and this method.
     def wait(self):
         if self._schedule_runner is not None:
             self._schedule_runner.wait()
