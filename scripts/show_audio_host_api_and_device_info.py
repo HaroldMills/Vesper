085aef8f80bac74e56c50b4a853cf804f0e6c74a
HaroldMills <harold.mills@gmail.com>
2017-01-31 12:53:02 -0500

    Improved audio device enumeration script.
    Mainly separated input and output device enumeration.

--- scripts/show_audio_host_api_and_device_info.py
+++ scripts/show_audio_host_api_and_device_info.py
@@ -6,23 +6,29 @@ import pyaudio
 
 def _main():
     
-    p = pyaudio.PyAudio()
+    pa = pyaudio.PyAudio()
     
-    _show_host_apis(p)
+    _show_host_apis(pa)
     print()
     
-    _show_devices(p)
+    _show_input_devices(pa)
     print()
     
-    _show_default_input_device_info(p)
+    _show_default_input_device_info(pa)
     print()
-
-    p.terminate()
+    
+    _show_output_devices(pa)
+    print()
+    
+    _show_default_output_device_info(pa)
+    print()
+    
+    pa.terminate()
     
     
-def _show_host_apis(p):
+def _show_host_apis(pa):
     
-    host_api_count = p.get_host_api_count()
+    host_api_count = pa.get_host_api_count()
     
     if host_api_count == 0:
         print('no PortAudio host APIs found')
@@ -30,42 +36,67 @@ def _show_host_apis(p):
     else:
         print('PortAudio host APIs:')
         for i in range(host_api_count):
-            info = p.get_host_api_info_by_index(i)
+            info = pa.get_host_api_info_by_index(i)
             print('{} "{}"'.format(i, info['name']))
     
 
-def _show_devices(p):
+def _show_input_devices(pa):
+    _show_devices(pa, 'input', 'maxInputChannels')
     
-    device_count = p.get_device_count()
     
-    if device_count == 0:
-        print('no PortAudio devices found')
+def _show_devices(pa, type_name, num_channels_key):
+    
+    infos = [i for i in _get_device_infos(pa) if i[num_channels_key] != 0]
+    
+    if len(infos) == 0:
+        print('No {} devices found.'.format(type_name))
         
     else:
-        print('PortAudio devices:')
-        for i in range(device_count):
-            info = p.get_device_info_by_index(i)
-            _show_device_info(i, info)
+        print('{} devices:'.format(type_name.capitalize()))
+        for info in infos:
+            _show_device_info(info)
+            
+    
+def _get_device_infos(pa):
+    num_devices = pa.get_device_count()
+    return [pa.get_device_info_by_index(i) for i in range(num_devices)]
             
             
-def _show_device_info(index, info):
+def _show_device_info(info):
     print('{} {} "{}" {} {} {} {} {}'.format(
-        index, info['hostApi'], info['name'], info['maxInputChannels'],
+        info['hostApi'], info['index'], info['name'], info['maxInputChannels'],
         info['maxOutputChannels'], info['defaultSampleRate'],
         info['defaultLowInputLatency'], info['defaultHighInputLatency']))
             
             
-def _show_default_input_device_info(p):
+def _show_default_input_device_info(pa):
     
     try:
-        info = p.get_default_input_device_info()
+        info = pa.get_default_input_device_info()
         
     except IOError:
         print('No default input device available.')
         
     else:
         print('Default input device:')
-        _show_device_info(None, info)
+        _show_device_info(info)
+    
+    
+def _show_output_devices(pa):
+    _show_devices(pa, 'output', 'maxOutputChannels')
+    
+    
+def _show_default_output_device_info(pa):
+    
+    try:
+        info = pa.get_default_output_device_info()
+        
+    except IOError:
+        print('No default output device available.')
+        
+    else:
+        print('Default output device:')
+        _show_device_info(info)
     
     
 if __name__ == '__main__':
