3edac48ecd3030376bc28d848cefc459519e1c6d
HaroldMills <harold.mills@gmail.com>
2017-03-24 10:25:58 -0400

    Added initial version of keyboard command interpreter.

---
+++ vesper/django/app/static/vesper/keyboard-command-interpreter.js
@@ -0,0 +1,403 @@
+'use strict'
+
+
+/*
+
+Terms:
+
+* function
+* environment
+* variable
+* global environment
+* local environment
+* action - function and arguments
+* command - name and action
+
+*/
+
+
+/*
+
+globals:
+    annotation_name: Classification
+    annotation_scope: Selected
+    
+commands:
+
+    "_": [clear_command_and_locals]
+    
+    ">": [show_next_page]
+    "<": [show_previous_page]
+    "^": [select_first_clip]
+    ".": [select_next_clip]
+    ",": [select_previous_clip]
+    "/": [play_selected_clip]
+    
+    "#": [set_local, scope, Page]
+    "*": [set_local, scope, All]
+
+    c: [annotate_clips, Call]
+    C: [annotate_page_clips, Call]
+    n: [annotate_clips, Noise]
+    N: [annotate_page_clips, Noise]
+    x: [unannotate_clips]
+    X: [unannotate_page_clips]
+    
+    r: [tag_clips, Review]
+    u: [untag_clips]
+
+*/
+
+
+class KeyboardCommandInterpreter {
+	
+	
+	constructor(spec, contributedFunctions) {
+		
+		this._functions = this._getFunctions(contributedFunctions);
+		
+		this._environment = new Environment();
+		this._initGlobals(spec);
+		
+		this._commandActions = this._getCommandActions(spec);
+		this._commandNamePrefixes = this._getCommandNamePrefixes();
+
+		this._clearCommandNameBuffer();
+		
+	}
+	
+	
+	_getFunctions(contributedFunctions) {
+		const interpreterFunctions = this._createInterpreterFunctions();
+		contributedFunctions = contributedFunctions.map((f) => [f.name, f]);
+		return new Map([...interpreterFunctions, ...contributedFunctions]);
+	}
+		
+		
+	_createInterpreterFunctions() {
+		
+		const functionData = [
+			['set_global', ['name', 'value'], this._setGlobal],
+			['delete_global', ['name'], this._deleteGlobal],
+			['clear_globals', [], this._clearGlobals],
+			['set_local', ['name', 'value'], this._setLocal],
+			['delete_local', ['name'], this._deleteLocal],
+			['clear_locals', [], this._clearLocals],
+			['clear_command_and_locals', [], this._clearCommandAndLocals]
+		]
+		
+		const entries = functionData.map(this._createInterpreterFunctionEntry);
+		return new Map(entries);
+		
+	}
+	
+	
+	_createInterpreterFunctionEntry(functionData) {
+		const name = functionData[0];
+		const value = new InterpreterFunction(...functionData);
+		return [name, value];
+	}
+
+	
+	_setGlobal(environment, name, value) {
+		environment.setGlobal(name, value);
+	}
+	
+	
+	_deleteGlobal(environment, name) {
+		environment.deleteGlobal(name);
+	}
+	
+	
+	_clearGlobals(environment) {
+		environment.clearGlobals();
+	}
+	
+	
+	_setLocal(environment, name, value) {
+		environment.setLocal(name, value);
+	}
+	
+	
+	_deleteLocal(environment, name) {
+		environment.deleteLocal(name);
+	}
+	
+	
+	_clearLocals(environment) {
+		environment.clearLocals();
+	}
+	
+	
+	_clearCommandAndLocals(environment) {
+		this._clearCommandNameBuffer();
+		environment.clearLocals();
+	}
+	
+	
+	_initGlobals(spec) {
+		
+		if (spec.globals) {
+			
+			for (const name of Object.keys(spec.globals)) {
+				const value = spec.globals[name];
+				this._environment.setGlobal(name, value);
+			}
+			
+		}
+		
+	}
+	
+	
+	_getCommandActions(spec) {
+		
+		const actions = new Map();
+		
+		if (spec.commands)
+			for (const name of Object.keys(spec.commands)) {
+				actions.set(name, spec.commands[name]);
+			}
+		
+		return actions;
+		
+	}
+	
+	
+	// Gets the set of all proper prefixes of this interpreter's command names.
+	_getCommandNamePrefixes() {
+		
+		const prefixes = new Set();
+		
+		for (const name of this._commandActions.keys())
+			for (let i = 1; i < name.length; i++)
+				prefixes.add(name.slice(0, i));
+	
+	    return prefixes;
+	    
+	}
+	
+	
+	_clearCommandNameBuffer() {
+		this._commandNameBuffer = '';
+	}
+	
+	
+	onKey(key) {
+		
+		console.log('onKey', key, this._commandNameBuffer);
+		
+		const name = this._commandNameBuffer + key;
+		const action = this._commandActions.get(name);
+		
+		if (action) {
+			// `name` is a command name
+			
+			this._executeCommand(name, action);
+			this._clearCommandNameBuffer();
+			
+		} else if (this._commandNamePrefixes.has(name)) {
+			// `name` is a proper prefix of one or more command names
+			
+			this._commandNameBuffer = name;
+			
+		} else {
+			// `name` is not a prefix of any command name
+			
+			// TODO: Notify user of error.
+			console.log(`Unrecognized command name "${name}".`);
+			this._clearCommandNameBuffer();
+			
+		}
+				
+	}
+	
+	
+	_executeCommand(name, action) {
+		
+		const function_name = action[0];
+		const function_arguments = action.slice(1);
+		
+		const function_ = this._functions.get(function_name);
+		
+		if (function_) {
+			
+			try {
+				
+			    function_.execute(function_arguments, this._environment);
+		
+			} catch (e) {
+				
+		        // TODO: Notify user of error.
+		        console.log(
+		        	`Execution of command "${name}" failed with ` +
+		        	`message: ${e.message}`);
+		        
+			}
+			
+		} else {
+			
+			// TODO: Notify user of error.
+			console.log(
+				`Could not find function "${function_name}" for ` +
+				`command "${name}".`);
+			
+		}
+
+	}
+	
+	
+}
+
+
+class _Function {
+	
+
+	constructor(name, argumentNames, executionDelegate) {
+		this._name = name;
+		this._argumentNames = argumentNames;
+		this._executionDelegate = executionDelegate;
+	}
+	
+	
+	get name() {
+		return this._name;
+	}
+	
+	
+	get argumentNames() {
+		return this._argumentNames;
+	}
+	
+	
+	get executionDelegate() {
+		return this._executionDelegate;
+	}
+
+
+	execute(argumentValues, environment) {
+		this._checkArguments(argumentValues);
+		this._execute(argumentValues, environment);
+	}
+	
+	
+	_checkArguments(argumentValues) {
+		
+		const numArgs = this.argumentNames.length;
+		
+		if (argumentValues.length !== numArgs)
+			throw new Error(
+				`Function "${this.name}" requires ${numArgs} ` +
+				`arguments but received ${argumentValues.length}.`);
+		
+	}
+
+	
+	_execute(argumentValues, environment) {
+		throw Error('The "_execute" method is not implemented.');
+	}
+	
+	
+}
+
+
+/*
+ * An `InterpreterFunction` simply invokes its execution delegate.
+ * It does not manipulate the local environment itself: any such
+ * manipulation is left to the delegate.
+ */
+class InterpreterFunction extends _Function {
+	
+	_execute(argumentValues, environment) {
+		this._executionDelegate(environment, ...argumentValues);
+	}
+	
+}
+
+
+/*
+ * A `ContributedFunction` adds arguments to the local environment before
+ * invoking the execution delegate, and clears the local environment after
+ * the execution delegate completes.
+ */
+class ContributedFunction extends _Function {
+	
+	_execute(argumentValues, environment) {
+		
+		// Add arguments to local variables.
+		for (const [i, name] of this.argumentNames.entries())
+			environment.setLocal(name, argumentValues[i]);
+		
+		try {
+			
+		    this._executionDelegate(environment);
+					
+		} finally {
+		
+		    // Clear local variables. Note that this includes *all* local
+			// variables, such as any that may have been set via the
+			// `set_local` function, not just the arguments of
+			// this function.
+		    environment.clearLocals();
+		    
+		}
+		
+	}
+
+}
+
+
+class Environment {
+	
+	
+	constructor() {
+		this._globals = new Map();
+		this._locals = new Map();
+	}
+	
+	
+    setGlobal(name, value) {
+    	this._globals.set(name, value);
+    }
+    
+    
+    deleteGlobal(name) {
+    	delete this._globals.delete(name);
+    }
+    
+    
+    clearGlobals() {
+    	this._globals.clear();
+    }
+    
+    
+    setLocal(name, value) {
+    	this._locals.set(name, value);
+    }
+    
+    
+    deleteLocal(name) {
+    	delete this._locals.delete(name);
+    }
+    
+    
+    clearLocals() {
+    	this._locals.clear();
+    }
+    
+    
+    get(name) {
+    	
+        let value = this._locals.get(name);
+    	if (value !== undefined)
+    		return value;
+    	
+    	value = this._globals.get(name);
+        if (value !== undefined)
+    		return value;
+    	    
+    	throw Error(`Variable "${name}" is not defined.`);
+    	    
+    }
+	
+	
+}
---
+++ vesper/django/app/static/vesper/tests/keyboard-command-interpreter-tests.js
@@ -0,0 +1,359 @@
+'use strict'
+
+
+describe('Environment class', () => {
+	
+	
+	it('globals', () => {
+		
+		const e = new Environment();
+		
+		e.setGlobal('one', 1);
+		e.setGlobal('two', 2);
+		e.setGlobal('three', 3);
+		
+		expect(e.get('one')).toBe(1);
+		expect(e.get('two')).toBe(2);
+		expect(e.get('three')).toBe(3);
+		
+		e.deleteGlobal('two');
+		
+		expect(e.get('one')).toBe(1);
+		expect(() => e.get('two')).toThrowError(Error);
+		expect(e.get('three')).toBe(3);
+		
+		e.clearLocals();
+		
+		expect(e.get('one')).toBe(1);
+		expect(e.get('three')).toBe(3);
+		
+		e.clearGlobals();
+		
+		expect(() => e.get('one')).toThrowError(Error);
+		expect(() => e.get('three')).toThrowError(Error);
+		
+	});
+	
+	
+	it('locals', () => {
+		
+		const e = new Environment();
+		
+		e.setLocal('one', 1);
+		e.setLocal('two', 2);
+		e.setLocal('three', 3);
+		
+		expect(e.get('one')).toBe(1);
+		expect(e.get('two')).toBe(2);
+		expect(e.get('three')).toBe(3);
+		
+		e.deleteLocal('two');
+		
+		expect(e.get('one')).toBe(1);
+		expect(() => e.get('two')).toThrowError(Error);
+		expect(e.get('three')).toBe(3);
+		
+		e.clearGlobals();
+		
+		expect(e.get('one')).toBe(1);
+		expect(e.get('three')).toBe(3);
+		
+		e.clearLocals();
+		
+		expect(() => e.get('one')).toThrowError(Error);
+		expect(() => e.get('three')).toThrowError(Error);
+		
+	});
+	
+	
+	it('globals and locals', () => {
+		
+		const e = new Environment();
+		
+		e.setGlobal('one', 1);
+		e.setGlobal('two', 2);
+		e.setLocal('three', 3);
+		e.setLocal('four', 4);
+		
+		expect(e.get('one')).toBe(1);
+		expect(e.get('two')).toBe(2);
+		expect(e.get('three')).toBe(3);
+        expect(e.get('four')).toBe(4);
+        
+        e.clearLocals();
+
+		expect(e.get('one')).toBe(1);
+		expect(e.get('two')).toBe(2);
+		expect(() => e.get('three')).toThrowError(Error);
+		expect(() => e.get('four')).toThrowError(Error);
+        
+		e.setLocal('three', 3);
+		e.setLocal('four', 4);
+		e.clearGlobals();
+		
+		expect(() => e.get('one')).toThrowError(Error);
+		expect(() => e.get('two')).toThrowError(Error);
+		expect(e.get('three')).toBe(3);
+        expect(e.get('four')).toBe(4);
+		
+	});
+	
+
+});
+
+
+describe('InterpreterFunction class', () => {
+	
+	
+	it('construction and execution', () => {
+		
+		function setLocal(environment, name, value) {
+			environment.setLocal(name, value);
+		}
+		
+		const f = new InterpreterFunction(
+			'set_local', ['name', 'value'], setLocal);
+		
+		const e = new Environment();
+		
+		expect(f.name).toBe('set_local');
+		expect(f.argumentNames).toEqual(['name', 'value']);
+		expect(f.executionDelegate).toBe(setLocal);
+		
+		f.execute(['bobo', 1], e);
+		
+		expect(e.get('bobo')).toBe(1);
+		
+	});
+	
+	
+});
+
+
+describe('ContributedFunction class', () => {
+	
+	
+	it('construction and execution', () => {
+		
+		function setGlobal(environment) {
+			const name = environment.get('name');
+			const value = environment.get('value');
+			environment.setGlobal(name, value);
+		}
+		
+		const f = new ContributedFunction(
+			'set_global', ['name', 'value'], setGlobal);
+		
+		const e = new Environment();
+		
+		expect(f.name).toBe('set_global');
+		expect(f.argumentNames).toEqual(['name', 'value']);
+		expect(f.executionDelegate).toBe(setGlobal);
+		
+		e.setLocal('local', 1);
+		f.execute(['bobo', 2], e);
+		
+		expect(() => e.get('local')).toThrowError(Error);
+		expect(e.get('bobo')).toBe(2);
+		
+	});
+	
+	
+});
+
+
+describe('KeyboardCommandInterpreter class', () => {
+	
+	
+	it('interpreter functions', () => {
+		
+		const spec = {
+			
+			'commands': {
+				
+				'sg1': ['set_global', 'one', 1],
+				'sg2': ['set_global', 'two', 2],
+				'dg1': ['delete_global', 'one'],
+				'dg2': ['delete_global', 'two'],
+				'cg': ['clear_globals'],
+				
+				'sl2': ['set_local', 'two', 2],
+				'sl3': ['set_local', 'three', 3],
+				'dl2': ['delete_local', 'two'],
+				'dl3': ['delete_local', 'three'],
+				'cl': ['clear_locals']
+				
+			}
+		
+		};
+		
+		const functions = [];
+				
+		const interpreter = new KeyboardCommandInterpreter(spec, functions);
+		const environment = interpreter._environment;
+		
+		function input(command) {
+			for (const c of command)
+				interpreter.onKey(c);
+		}
+		
+		function expectValue(name, value) {
+			expect(environment.get(name)).toBe(value);
+		}
+		
+		function expectUndefined(name) {
+			expect(() => environment.get(name)).toThrowError(Error);
+		}
+
+		
+		// globals
+		
+		input('sg1');
+		input('sg2');
+		
+		expectValue('one', 1);
+		expectValue('two', 2);
+		
+		input('dg1');
+		
+		expectUndefined('one');
+		expectValue('two', 2);
+		
+		input('sg1');
+		
+		expectValue('one', 1);
+		expectValue('two', 2);
+		
+		input('cg');
+		
+		expectUndefined('one');
+		expectUndefined('two');
+		
+		
+		// locals
+		
+		input('sl2');
+		input('sl3');
+		
+		expectValue('two', 2);
+		expectValue('three', 3);
+		
+		input('dl2');
+		
+		expectUndefined('two');
+		expectValue('three', 3);
+		
+		input('sl2');
+		
+		expectValue('two', 2);
+		expectValue('three', 3);
+		
+		input('cl');
+		
+		expectUndefined('two');
+		expectUndefined('three');
+
+		
+		// globals and locals
+		
+		input('sg1');
+		input('sg2');
+		input('sl2');
+		input('sl3');
+		
+		expectValue('one', 1);
+		expectValue('two', 2);
+		expectValue('three', 3);
+		
+		input('cl');
+		
+		expectValue('one', 1);
+		expectValue('two', 2);
+		expectUndefined('three');
+		
+		
+	});
+	
+	
+	it('contributed functions', () => {
+		
+		const spec = {
+			
+			'commands': {
+				's1': ['set', 'one', 1],
+				's2': ['set', 'two', 2],
+				's3': ['set_three'],
+			    'sl4': ['set_local', 'four', 4],
+			    'sg5': ['set_global', 'five', 5]
+			},
+		
+		    'globals': {
+		    	'name': 'three',
+		    },
+		    		
+		};
+		
+		const values = new Map();
+		
+		function set(environment) {
+			const name = environment.get('name');
+			const value = environment.get('value');
+			values.set(name, value);
+		}
+		
+		const functions = [
+			new ContributedFunction('set', ['name', 'value'], set),
+			new ContributedFunction('set_three', ['value'], set)
+		];
+				
+		const interpreter = new KeyboardCommandInterpreter(spec, functions);
+		const environment = interpreter._environment;
+		
+		function input(command) {
+			for (const c of command)
+				interpreter.onKey(c);
+		}
+		
+		function expectSetValue(name, value) {
+			expect(values.get(name) === value);
+		}
+		
+		function expectValue(name, value) {
+			expect(environment.get(name)).toBe(value);
+		}
+		
+		function expectUndefined(name) {
+			expect(() => environment.get(name)).toThrowError(Error);
+		}
+
+		function expectEnv() {
+			expectUndefined('one');
+			expectUndefined('two');
+			expectUndefined('three');
+			expectUndefined('four');
+			expectValue('five', 5);
+			expectValue('name', 'three');
+			expectUndefined('value');
+		}
+		
+		input('sl4');
+		input('sg5');
+		
+		input('s1');
+		expectSetValue('one', 1);
+		expectEnv();
+		
+		input('s2');
+		expectSetValue('one', 1);
+		expectSetValue('two', 2);
+		expectEnv();
+		
+		input('s3');
+		expectSetValue('one', 1);
+		expectSetValue('two', 2);
+		expectSetValue('three', 3);
+		expectEnv();
+		
+	});
+	
+});
