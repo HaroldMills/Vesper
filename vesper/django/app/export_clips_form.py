dd0612bc28ce2577b9efc3bc1e7ae521b0ce08e3
HaroldMills <harold.mills@gmail.com>
2017-04-06 14:47:11 -0400

    Changed order of detector and station/mic choices in clip export form.

--- vesper/django/app/export_clips_form.py
+++ vesper/django/app/export_clips_form.py
@@ -8,18 +8,21 @@ from vesper.django.app.models import Processor, Station
 class ExportClipsForm(forms.Form):
     
 
+    detectors = forms.MultipleChoiceField(label='Detectors')
+
     station_mics = forms.MultipleChoiceField(
-        label='Station/microphone pairs',
+        label='Station/mics',
         help_text='''
-            This is the station/microphone help text. I'm going to make it
+            This is the station/mic help text. I'm going to make it
             rather long so we can see how such text is displayed. I hope
             the display is reasonable. If it isn't perhaps we can develop
             a help text formatting function that can pre-process the text
             to make it look better.''')
     
-    detectors = forms.MultipleChoiceField(label='Detectors')
     start_date = forms.DateField(label='Start date')
+    
     end_date = forms.DateField(label='End date')
+    
     output_dir_path = forms.CharField(
         label='Output directory', max_length=255,
         widget=forms.TextInput(attrs={'class': 'command-form-wide-input'}))
@@ -29,16 +32,16 @@ class ExportClipsForm(forms.Form):
         
         super().__init__(*args, **kwargs)
         
+        # Populate detectors field.
+        detectors = Processor.objects.filter(
+            algorithm_version__algorithm__type='Detector')
+        self.fields['detectors'].choices = [(d.name, d.name) for d in detectors]
+
         # Populate stations field.
         stations = Station.objects.all()
         choices = list(itertools.chain.from_iterable(
             _get_station_mic_choices(s) for s in stations))
         self.fields['station_mics'].choices = choices
-
-        # Populate detectors field.
-        detectors = Processor.objects.filter(
-            algorithm_version__algorithm__type='Detector')
-        self.fields['detectors'].choices = [(d.name, d.name) for d in detectors]
 
 
 def _get_station_mic_choices(station):
