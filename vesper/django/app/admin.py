55c4b5047239bfb6aa7da8f9dbffc597464d7fa1
HaroldMills <harold.mills@gmail.com>
2017-05-04 17:05:59 -0400

    Added job and recording channel models to admin UI.

--- vesper/django/app/admin.py
+++ vesper/django/app/admin.py
@@ -3,14 +3,14 @@ from django.contrib import admin
 from vesper.django.app.models import (
     AnnotationConstraint, AnnotationInfo, Clip, Device, DeviceConnection,
     DeviceInput, DeviceModel, DeviceModelInput, DeviceModelOutput,
-    DeviceOutput, Processor, Recording, RecordingFile, Station, StationDevice,
-    StringAnnotation)
+    DeviceOutput, Job, Processor, Recording, RecordingChannel, RecordingFile,
+    Station, StationDevice, StringAnnotation)
 
 classes = (
     AnnotationConstraint, AnnotationInfo, Clip, Device, DeviceConnection,
     DeviceInput, DeviceModel, DeviceModelInput, DeviceModelOutput,
-    DeviceOutput, Processor, Recording, RecordingFile, Station, StationDevice,
-    StringAnnotation)
+    DeviceOutput, Job, Processor, Recording, RecordingChannel, RecordingFile,
+    Station, StationDevice, StringAnnotation)
                              
 for cls in classes:
     admin.site.register(cls)
