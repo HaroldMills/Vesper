412e95e4c7b060ebaf41884935e8207f02602e3e
HaroldMills <harold.mills@gmail.com>
2017-08-11 16:26:28 -0400

    Edited comments in archive lock module.

--- vesper/util/archive_lock.py
+++ vesper/util/archive_lock.py
@@ -6,12 +6,12 @@ archives from more than one thread and/or process. For the time
 being, at least, we have resorted to using such a lock since we
 have had problems with concurrent SQLite database transactions
 raising Django `OperationalError` exceptions with the message
-"database is locked". Proper use of the lock prevents these
-errors by ensuring that only one transaction is attempted at
-a time.
+"database is locked". Proper use of the archive lock prevents
+these errors by ensuring that only one transaction is attempted
+at a time.
 
 To use the lock, any code that writes to an archive should run
-inside a Python `with` statement whose context manager is the loxk.
+inside a Python `with` statement whose context manager is the lock.
 Note that when a transaction surrounds a sequence of writes, the
 transaction should be initiated only *after* acquiring the lock.
 
@@ -26,20 +26,22 @@ I'm not sure it will work for, say, uWSGI deployments that handle
 requests with multiple processes.]
 
 Use of the lock slows database writes, but without it writes to an
-SQLite archive database fail if more than one thread and/or process
-attempts to write to the database at once. For example, we have seen
-this problem when trying to classify all of the clips of a clip album
-page with a single keystroke, and even when trying to classify multiple
-clips with one keystroke per clip on a slow computer. Even though each
-clip classification was attempted on the server in its own database
-transaction, assuring atomicity, many classification requests (one for
-each clip) went to the server in quick succession, resulting in multiple
-concurrent transactions, some of which failed. A Django `OperationalError`
+SQLite archive database sometimes fail if more than one thread and/or
+process attempts to write to the database at once. For example, we have
+seen this problem when trying to classify all of the clips of a clip
+album page with a single keystroke (such a keystroke used to initiate
+multiple annotation requests to the server: now it initiates only one),
+and even when trying to classify multiple clips with one keystroke per
+clip on a slow computer. Even though each clip classification was
+attempted on the server in its own database transaction, assuring
+atomicity, many classification requests (one for each clip) went to
+the server in quick succession, resulting in multiple concurrent
+transactions, some of which failed. A Django `OperationalError`
 exception was raised for each failed transaction, with the message
 "database is locked".
 
-Vesper archives that use a database engine (perhaps PostgreSQL, for
-example) that supports concurrent writes better than SQLite may not
+Vesper archives that use a database engine that supports concurrent
+writes better than SQLite (perhaps PostgreSQL, for example) may not
 require this lock. In that case, we could make the value of the
 `archive_lock` attribute of this module an instance of our own
 context manager that can behave differently for different database
@@ -56,10 +58,10 @@ https://docs.djangoproject.com/en/1.11/ref/databases/#sqlite-notes),
 but that also resulted in "database is locked" errors. The errors
 started appearing more or less immediately after a keystroke to
 classify all of the clips of a page, rather than one second after,
-which suggests to me that the problem isn't just that the database
-has limited support for concurrent transactions, but rather that it
-does not support concurrent transactions at all, at least as
-configured here.
+which suggests to me that the problem may not be just that the
+database has limited support for concurrent transactions, but
+rather that it does not support concurrent transactions at all,
+at least as configured here.
 """
 
 
