b1684d32312ab9bacb0249bdce69d0ccb8edbc6f
HaroldMills <harold.mills@gmail.com>
2017-11-15 14:48:17 -0600

    Merged 2.0 tseep and thrush coarse classifiers into one.

--- vesper/mpg_ranch/nfc_coarse_classifier_2_0/classifier.py
+++ vesper/mpg_ranch/nfc_coarse_classifier_2_0/classifier.py
@@ -1,11 +1,10 @@
 """
 Module containing MPG Ranch NFC coarse classifier, version 2.0.
 
-An NFC coarse classifier classifies an unclassified clip of the
-classifier's clip type (either `'Tseep'` or `'Thrush'`) as a `'Call'`
-if it appears to be a nocturnal flight call, and as a `'Noise'` otherwise.
-The classifier does not alter the classification of a clip if the clip
-is not of the classifier's clip type, or if it is not unclassified.
+An NFC coarse classifier classifies an unclassified clip as a `'Call'`
+if it appears to be a nocturnal flight call, or as a `'Noise'` otherwise.
+It does not classify a clip that has already been classified, whether
+manually or automatically.
 """
 
 
@@ -21,12 +20,51 @@ import vesper.mpg_ranch.nfc_coarse_classifier_2_0.classifier_utils as \
     classifier_utils
 
 
-class _Classifier(Annotator):
+class Classifier(Annotator):
     
     
-    def __init__(self, clip_type, *args, **kwargs):
+    extension_name = 'MPG Ranch NFC Coarse Classifier 2.0'
+
+    
+    def __init__(self, *args, **kwargs):
+        
         super().__init__(*args, **kwargs)
-        self.clip_type = clip_type
+        
+        self._classifiers = dict(
+            (t, _Classifier(t)) for t in ('Thrush', 'Tseep'))
+        
+        
+    def annotate(self, clip):
+        
+        annotated = False
+        
+        classification = self._get_annotation_value(clip)
+        
+        if classification is None:
+            # clip is unclassified
+            
+            clip_type = model_utils.get_clip_type(clip)
+            classifier = self._classifiers.get(clip_type)
+        
+            if classifier is not None:
+                # clip is of a type for which we have a classifier
+                
+                classification = classifier.classify(clip)
+                
+                if classification is not None:
+                    self._annotate(clip, classification)
+                    annotated = True
+                    
+        return annotated
+    
+
+class _Classifier:
+    
+    
+    def __init__(self, clip_type):
+        
+        self._clip_type = clip_type
+        
         self._model = self._load_model()
         self._settings = self._load_settings()
         self._feature_computer = FeatureComputer(self._settings)
@@ -40,41 +78,18 @@ class _Classifier(Annotator):
         # we are about to use it.
         import keras
         
-        path = classifier_utils.get_model_file_path(self.clip_type)
+        path = classifier_utils.get_model_file_path(self._clip_type)
         return keras.models.load_model(path)
     
     
     def _load_settings(self):
-        path = classifier_utils.get_settings_file_path(self.clip_type)
+        path = classifier_utils.get_settings_file_path(self._clip_type)
         text = path.read_text()
         d = yaml.load(text)
         return Settings.create_from_dict(d)
         
         
-    def annotate(self, clip):
-        
-        annotated = False
-        
-        classification = self._get_annotation_value(clip)
-        
-        if classification is None:
-            # clip is unclassified
-            
-            clip_type = model_utils.get_clip_type(clip)
-        
-            if clip_type == self.clip_type:
-                # clip is of this classifier's type
-                
-                classification = self._classify(clip)
-                
-                if classification is not None:
-                    self._annotate(clip, classification)
-                    annotated = True
-                    
-        return annotated
-    
-    
-    def _classify(self, clip):
+    def classify(self, clip):
         
         waveform = self._get_waveform(clip)
         
@@ -115,19 +130,3 @@ class _Classifier(Annotator):
                 waveform, sample_rate, classifier_sample_rate)
             
         return waveform
-
-
-class ThrushClassifier(_Classifier):
-    
-    extension_name = 'MPG Ranch Thrush Coarse Classifier 2.0'
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__('Thrush', *args, **kwargs)
-
-
-class TseepClassifier(_Classifier):
-    
-    extension_name = 'MPG Ranch Tseep Coarse Classifier 2.0'
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__('Tseep', *args, **kwargs)
