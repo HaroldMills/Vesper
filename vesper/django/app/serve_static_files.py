410261b1109733ec666f04efd4c22a4268581f39
Harold Mills <harold.mills@gmail.com>
2018-02-13 17:24:17 -0500

    Added script to serve static client files using Flask.

---
+++ vesper/django/app/serve_static_files.py
@@ -0,0 +1,18 @@
+# Script that serves static Vesper client files using Flask.
+#
+# I created this script in February, 2018 to help troubleshoot occasional
+# hangs that occurred when running client unit tests using Python's
+# `http.server`. I found that the hangs can also occur when using Flask.
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/<path:path>')
+def static_file(path):
+    return app.send_static_file(path)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
