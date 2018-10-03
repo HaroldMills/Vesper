d870eed19ef65f13c3500aa98a60bb28dc10fdc8
Harold Mills <harold.mills@gmail.com>
2018-10-03 10:19:17 -0400

    Incremented Vesper version number to 0.4.4.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(
       
     name='vesper',
-    version='0.4.3',
+    version='0.4.4',
     description=(
         'Software for acoustical monitoring of nocturnal bird migration.'),
     url='https://github.com/HaroldMills/Vesper',
