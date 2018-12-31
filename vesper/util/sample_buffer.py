a9fe028691d3aa3abf824b367ef8b09e4687daa6
Harold Mills <harold.mills@gmail.com>
2018-12-31 11:01:34 -0500

    Added FIFO sample buffer class.

---
+++ vesper/util/sample_buffer.py
@@ -0,0 +1,254 @@
+"""Module containing `SampleBuffer` class."""
+
+
+from collections import deque
+
+import numpy as np
+
+
+class SampleBuffer:
+    
+    """
+    FIFO sample buffer.
+    
+    A `SampleBuffer` decouples sample production from sample consumption,
+    enabling a producer and a consumer to write and read samples in
+    whatever way is most convenient for them.
+    
+    A *sample producer* writes a sequence of NumPy sample arrays to a
+    `SampleBuffer` via the buffer's `write` method. The arrays can be of
+    various sizes. The buffer retains samples written by the producer
+    until the consumer indicates (by incrementing the buffer's read index:
+    see below) that it no longer needs them.
+    
+    A *sample consumer* reads the samples written by the producer via
+    the buffer's `read` method. Each call to the `read` method returns
+    one NumPy sample array. The consumer can read different numbers of
+    samples in different `read` calls, and the read sizes are independent
+    of the producer's write sizes. Each read starts at the *read index*
+    of the buffer. The read index starts at zero and is incremented by
+    the consumer, either via the `read` method or via the separate
+    `increment` method. The only constraint on the read index is that
+    it cannot be decremented: it can only be incremented or left
+    unchanged. By controlling the read index appropriately, the
+    consumer can read consecutive, overlapping, or non-consecutive and
+    non-overlapping sample arrays.
+    """
+    
+    
+    def __init__(self, dtype):
+        
+        """
+        Initializes this buffer.
+        
+        The buffer is empty after initialization, with a write index,
+        read index, and length of zero.
+        
+        Parameters
+        ----------
+        dtype : NumPy dtype
+            the `dtype` of this buffer.
+        """
+        
+        self._dtype = np.dtype(dtype)
+        self._arrays = deque()
+        self._stored_start_index = 0
+        self._stored_length = 0
+        self._read_index = 0
+        
+        
+    @property
+    def dtype(self):
+        return self._dtype
+    
+    
+    @property
+    def _stored_end_index(self):
+        return self._stored_start_index + self._stored_length
+    
+    
+    @property
+    def write_index(self):
+        return self._stored_end_index
+    
+    
+    @property
+    def read_index(self):
+        return self._read_index
+    
+    
+    def __len__(self):
+        return max(self._stored_end_index - self._read_index, 0)
+    
+    
+    def write(self, samples):
+        
+        """
+        Writes the specified samples to this buffer.
+        
+        Parameters
+        ----------
+        samples : NumPy array
+            the samples to be written.
+            
+            The sample array should not be modified after writing
+            until the buffer's read index has been incremented past it.
+            
+        Raises
+        ------
+        ValueError
+            if the `dtype` of `samples` differs from that of this buffer.
+        """
+        
+        
+        if samples.dtype != self._dtype:
+            
+            raise ValueError((
+                'NumPy dtype "{}" of samples does not match sample '
+                'buffer dtype "{}".').format(
+                    str(samples.dtype), str(self._dtype)))
+            
+        else:
+            # `samples` dtype matches buffer dtype
+            
+            if len(samples) != 0:
+                self._arrays.append(samples)
+                self._stored_length += len(samples)
+        
+        
+    def read(self, num_samples, increment=None):
+        
+        """
+        Reads samples from this buffer.
+        
+        Parameters
+        ----------
+        num_samples : int
+            the number of samples to read
+            
+        increment : int or None
+            the number by which to increment the read index of this buffer,
+            or `None` to increment it by `num_samples`.
+            
+        Returns
+        -------
+        NumPy array of samples, of this buffer's `dtype`.
+        
+        Raises
+        ------
+        ValueError
+            if `num_samples` or `increment` is negative, or if `num_samples`
+            exceeds the current buffer length.
+        """
+        
+        if increment is None:
+            increment = num_samples
+            
+        elif increment < 0:
+            raise ValueError(
+                'Sample buffer read increment cannot be negative.')
+        
+        if num_samples < 0:
+            raise ValueError('Sample buffer read size cannot be negative.')
+        
+        elif num_samples == 0:
+            result = np.array([], dtype=self._dtype)
+        
+        elif num_samples > len(self):
+            # don't have requested samples
+            
+            raise ValueError((
+                'Attempt to read {} samples from sample buffer with '
+                'only {}.').format(num_samples, len(self)))
+        
+        else:
+            # have requested samples
+            
+            result_arrays = []
+            array_num = 0
+            start = self.read_index - self._stored_start_index
+            n = num_samples
+            
+            while n != 0:
+                
+                array = self._arrays[array_num]
+                length = len(array)
+                available = length - start
+                
+                if n >= available:
+                    # result will include rest of `array`
+                    
+                    result_arrays.append(array[start:])
+                    array_num += 1
+                    start = 0
+                    n -= available
+
+                else:
+                    # result will include only part of rest of `array`
+                    
+                    result_arrays.append(array[start:start + n])
+                    n -= n
+                    
+            result = np.concatenate(result_arrays)
+            
+        if increment != 0:
+            self.increment(increment)
+            
+        return result
+        
+        
+    def increment(self, num_samples):
+        
+        """
+        Increments the read index of this buffer.
+        
+        Parameters
+        ----------
+        num_samples : int
+            the number of samples by which to increment the read index.
+            
+        Raises
+        ------
+        ValueError
+            if `num_samples` is negative.
+        """
+        
+        if num_samples < 0:
+            raise ValueError(
+                'Sample buffer read increment cannot be negative.')
+            
+        else:
+            
+            if num_samples >= len(self):
+                # increment will skip over all available samples
+                
+                self._arrays.clear()
+                self._stored_start_index = self._stored_end_index
+                self._stored_length = 0
+                
+            else:
+                # increment will not skip over all available samples
+                
+                start = self.read_index - self._stored_start_index
+                n = num_samples
+                
+                while n != 0:
+                    
+                    length = len(self._arrays[0])
+                    available = length - start
+                    
+                    if n >= available:
+                        # increment will skip over rest of `array`
+                        
+                        self._arrays.popleft()
+                        self._stored_start_index += length
+                        self._stored_length -= length
+                        start = 0
+                        n -= available
+                        
+                    else:
+                        # increment will skip over only part of rest of `array`
+                        
+                        n -= n
+                    
+            self._read_index += num_samples
---
+++ vesper/util/tests/test_sample_buffer.py
@@ -0,0 +1,89 @@
+import numpy as np
+
+
+from vesper.tests.test_case import TestCase
+from vesper.util.sample_buffer import SampleBuffer
+
+
+class SampleBufferTests(TestCase):
+    
+    
+    def test_init(self):
+        b = SampleBuffer(np.int64)
+        self.assertEqual(b.dtype, np.int64)
+        self._assert_buffer(b, 0, 0, 0)
+        
+        
+    def _assert_buffer(self, buffer, write_index, read_index, length):
+        self.assertEqual(buffer.write_index, write_index)
+        self.assertEqual(buffer.read_index, read_index)
+        self.assertEqual(len(buffer), length)
+
+
+    def test_zero_length_write(self):
+        b = SampleBuffer(np.float64)
+        b.write(np.array([]))
+        self._assert_buffer(b, 0, 0, 0)
+        
+        
+    def test_zero_length_read(self):
+        b = SampleBuffer(np.float64)
+        result = b.read(0)
+        self._assert_arrays_equal(result, np.array([], np.float64))
+        self._assert_buffer(b, 0, 0, 0)
+        
+        
+    def test_simple_write_read(self):
+        b = SampleBuffer(np.int64)
+        b.write(np.arange(0, 10))
+        b.write(np.arange(10, 20))
+        self._assert_arrays_equal(b.read(20), np.arange(0, 20))
+                
+        
+    def test_write_read(self):
+        b = SampleBuffer(np.int64)
+        b.write(np.arange(0, 10))
+        b.write(np.arange(10, 20))
+        self._assert_buffer(b, 20, 0, 20)
+        self._assert_arrays_equal(b.read(7), np.arange(0, 7))
+        self._assert_buffer(b, 20, 7, 13)
+        self._assert_arrays_equal(b.read(7), np.arange(7, 14))
+        self._assert_buffer(b, 20, 14, 6)
+        self._assert_arrays_equal(b.read(6), np.arange(14, 20))
+        self._assert_buffer(b, 20, 20, 0)
+        
+        
+    def test_simple_increments(self):
+        b = SampleBuffer(np.int64)
+        b.increment(10)
+        b.increment(20)
+        self._assert_buffer(b, 0, 30, 0)
+        
+        
+    def test_write_read_inc(self):
+        
+        b = SampleBuffer(np.int64)
+        
+        b.increment(5)
+        b.write(np.arange(0, 10))
+        self._assert_arrays_equal(b.read(2), np.arange(5, 7))
+        self._assert_buffer(b, 10, 7, 3)
+        
+        b.write(np.arange(10, 20))
+        self._assert_arrays_equal(b.read(5), np.arange(7, 12))
+        self._assert_buffer(b, 20, 12, 8)
+
+        b.increment(5)
+        self._assert_arrays_equal(b.read(3), np.arange(17, 20))
+        self._assert_buffer(b, 20, 20, 0)
+        
+        b.increment(5)
+        self._assert_buffer(b, 20, 25, 0)
+        
+        
+    def test_read_with_inc(self):
+        b = SampleBuffer(np.int64)
+        b.write(np.arange(0, 20))
+        self._assert_arrays_equal(b.read(10, 5), np.arange(0, 10))
+        self._assert_arrays_equal(b.read(10, 5), np.arange(5, 15))
+        self._assert_arrays_equal(b.read(10, 5), np.arange(10, 20))
