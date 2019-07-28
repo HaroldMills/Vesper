7eb2427b82cfe5ab3b989ecd7ee1e7e387835139
HaroldMills <harold.mills@gmail.com>
2019-07-28 14:38:55 -0400

    Modified recording pruning script to use new station night sets module.

--- scripts/detector_eval/manual/prune_recordings.py
+++ scripts/detector_eval/manual/prune_recordings.py
@@ -1,18 +1,17 @@
 """
 Script that prunes the recordings of an archive.
 
-For each station in the archive of the directory in which this script is
-run, and each of the months specified by the `MONTH_NUMS` module attribute,
-the script randomly chooses one night for the station and month for which
-there is at least one recording (if there are any such nights) and deletes
-all recordings for the station and month for all other nights. For each
-station and month, this leaves only the recordings for the chosen night.
+For the set of station-nights assigned to `retained_station_nights`
+in the `main` function of this script, the script deletes from the
+archive of the directory in which it is run all recordings for all
+other station nights, leaving only the recordings of the specified
+station-nights.
 """
 
 
-from collections import defaultdict
 import os
-import random
+
+import scripts.detector_eval.manual.station_night_sets as station_night_sets
 
 
 # Set up Django.
@@ -23,75 +22,15 @@ django.setup()
 from vesper.django.app.models import Recording
 
 
-MONTH_NUMS = [9]
-"""
-Numbers of the months for which to retain recordings.
-
-For each station that has recordings for one or more nights of a specified
-month, the recordings for one randomly chosen night of the month are
-retained but the recordings for all other nights of the month are deleted.
-"""
-
-
 def main():
     
-    # Seed random number generator to make recording choices reproducible.
-    random.seed(0)
-    
-    night_sets = get_night_sets()
-    
-    retained_station_nights = choose_station_nights(night_sets, MONTH_NUMS)
+    retained_station_nights = station_night_sets.VALIDATION_STATION_NIGHTS
     
     show_station_nights(retained_station_nights)
     
     # Be careful about uncommenting the following line!
-    # delete_recordings(retained_station_nights)
+    delete_recordings(retained_station_nights)
         
-    
-def get_night_sets():
-    
-    """
-    Gets a mapping from station names to sets of nights for which there
-    are recordings.
-    """
-
-    night_sets = defaultdict(set)
-    
-    for recording in Recording.objects.all():
-        station_name, night = get_recording_info(recording)
-        night_sets[station_name].add(night)
-        
-    return night_sets
-        
-        
-def get_recording_info(recording):
-    station = recording.station
-    night = station.get_night(recording.start_time)
-    return station.name, night
-
-
-def choose_station_nights(night_sets, month_nums):
-    
-    station_nights = set()
-    
-    for station_name in night_sets.keys():
-        
-        nights = night_sets[station_name]
-        
-        for month_num in month_nums:
-            
-            # We sort the nights before choosing to make the choice
-            # reproducible across runs of this script.
-            month_nights = sorted(n for n in nights if n.month == month_num)
-            
-            if len(month_nights) != 0:
-                # there is at least one recording for this station and month
-                
-                night = random.choice(month_nights)
-                station_nights.add((station_name, night))
-            
-    return station_nights
-           
     
 def show_station_nights(station_nights):
     station_nights = sorted(station_nights)
@@ -109,9 +48,9 @@ def delete_recordings(retained_station_nights):
         # if recording.station.name != 'Angel':
         #     continue
         
-        info = get_recording_info(recording)
+        station_night = get_recording_station_night(recording)
         
-        if info not in retained_station_nights:
+        if station_night not in retained_station_nights:
             
             print('deleting recording "{}"...'.format(recording))
             recording.delete()
@@ -124,5 +63,11 @@ def delete_recordings(retained_station_nights):
         num_deleted, num_recordings - num_deleted, num_recordings))
 
     
+def get_recording_station_night(recording):
+    station = recording.station
+    night = station.get_night(recording.start_time)
+    return station.name, night
+
+
 if __name__ == '__main__':
     main()
