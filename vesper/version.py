513c5b1fd895ca1e7480fa23190208d06a4a4320
HaroldMills <harold.mills@gmail.com>
2020-03-12 10:48:04 -0400

    Incremented Vesper version number to 0.4.8.

--- vesper/version.py
+++ vesper/version.py
@@ -9,7 +9,7 @@ module that needs the Vesper version should obtain it from this module.
 major_number = 0
 minor_number = 4
 patch_number = 8
-suffix = 'rc3'
+suffix = ''
 
 major_version = f'{major_number}'
 minor_version = f'{major_version}.{minor_number}'
