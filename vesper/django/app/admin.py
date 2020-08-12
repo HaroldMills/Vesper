d8d44b3f4bd9494dbf4d43502d58ee0a26eed07c
harold <harold@harolds-mbp>
2020-08-12 12:13:15 -0400

    Added tag models to admin interface.

--- vesper/django/app/admin.py
+++ vesper/django/app/admin.py
@@ -4,13 +4,15 @@ from vesper.django.app.models import (
     AnnotationConstraint, AnnotationInfo, Clip, Device, DeviceConnection,
     DeviceInput, DeviceModel, DeviceModelInput, DeviceModelOutput,
     DeviceOutput, Job, Processor, Recording, RecordingChannel, RecordingFile,
-    Station, StationDevice, StringAnnotation, StringAnnotationEdit)
+    Station, StationDevice, StringAnnotation, StringAnnotationEdit, Tag,
+    TagEdit, TagInfo)
 
 classes = (
     AnnotationConstraint, AnnotationInfo, Clip, Device, DeviceConnection,
     DeviceInput, DeviceModel, DeviceModelInput, DeviceModelOutput,
     DeviceOutput, Job, Processor, Recording, RecordingChannel, RecordingFile,
-    Station, StationDevice, StringAnnotation, StringAnnotationEdit)
+    Station, StationDevice, StringAnnotation, StringAnnotationEdit, Tag,
+    TagEdit, TagInfo)
                              
 for cls in classes:
     admin.site.register(cls)
