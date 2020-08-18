3c173945f14ae3d4736d79c029667cba9b5ecbf6
harold <harold@harolds-mbp>
2020-08-18 12:52:23 -0400

    Modified clip HDF5 file export form to subclass ClipSetForm.

--- vesper/django/app/export_clips_to_hdf5_file_form.py
+++ vesper/django/app/export_clips_to_hdf5_file_form.py
@@ -1,36 +1,10 @@
 from django import forms
 
-import vesper.django.app.form_utils as form_utils
-import vesper.django.app.model_utils as model_utils
+from vesper.django.app.clip_set_form import ClipSetForm
 
 
-class ExportClipsToHdf5FileForm(forms.Form):
-    
-
-    detectors = forms.MultipleChoiceField(label='Detectors')
-    station_mics = forms.MultipleChoiceField(label='Station/mics')
-    classification = forms.ChoiceField(label='Classification')
-    start_date = forms.DateField(label='Start date')
-    end_date = forms.DateField(label='End date')
+class ExportClipsToHdf5FileForm(ClipSetForm):
     
     output_file_path = forms.CharField(
         label='Output file', max_length=255,
         widget=forms.TextInput(attrs={'class': 'command-form-wide-input'}))
-    
-    
-    def __init__(self, *args, **kwargs):
-        
-        super().__init__(*args, **kwargs)
-        
-        # Populate detectors field.
-        self.fields['detectors'].choices = \
-            form_utils.get_processor_choices('Detector')
-
-        # Populate station/mics field.
-        names = model_utils.get_station_mic_output_pair_ui_names()
-        choices = [(name, name) for name in names]
-        self.fields['station_mics'].choices = choices
-
-        # Populate classification field.
-        self.fields['classification'].choices = \
-            form_utils.get_string_annotation_value_choices('Classification')
