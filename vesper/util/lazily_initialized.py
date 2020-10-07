65b51f5cf450f790d25b7b24d9470e673f4cf8ca
harold <harold@harolds-mbp>
2020-10-07 14:35:00 -0400

    Edited lazily_initialized module.

--- vesper/util/lazily_initialized.py
+++ vesper/util/lazily_initialized.py
@@ -7,14 +7,11 @@ class LazilyInitialized:
     Superclass of objects that are initialized lazily.
     
     Some or all of the initialization of a lazily initialized object is
-    deferred until after its `__init__` method is called.
+    deferred from its `__init__` method until other methods are called.
     
-    Lazy initialization can be used to:
-    
-    * Increase program startup speed.
-    
-    * Avoid incurring the cost of initialization for an object until it
-      is actually used, saving that cost for objects that are never used.
+    Lazy initialization can be used to increase program startup speed
+    by deferring some initialization, and to save resources by avoiding
+    initialization of program components that might never be used.
     """
     
     
@@ -22,16 +19,17 @@ class LazilyInitialized:
     def initter(method):
         
         """
-        Decorator for `LazilyInitialized` instance method that initializes
-        an object if needed before invoking the decorated method on it.
+        Decorator for subclass instance methods that initializes if needed
+        before doing anything else.
+        
+        An instance method that needs to ensure that the object it's called
+        on is initialized can either be decorated with this decorator or it
+        can call `self._initialize_if_needed` directly before doing anything
+        else.
         """
         
         def wrapper(self, *args, **kwargs):
-            
-            if not self._initialized:
-                self._init()
-                self._initialized = True
-                
+            self._initialize_if_needed()
             return method(self, *args, **kwargs)
         
         return wrapper
@@ -41,6 +39,17 @@ class LazilyInitialized:
         self._initialized = False
         
         
+    def _initialize_if_needed(self):
+        
+        """
+        Initializes this object if needed by calling its `_init` method.
+        """
+        
+        if not self._initialized:
+            self._init()
+            self._initialized = True
+    
+    
     def _init(self):
         
         """
@@ -48,8 +57,15 @@ class LazilyInitialized:
         
         The default implementation of this method does nothing.
         Subclasses can override the method to perform their initialization.
-        An indirect `LazilyInitialized` subclass should always call
-        `super()._init` from its `_init` method.
+        Any indirect `LazilyInitialized` subclass (i.e. one that is not
+        a direct subclass of `LazyInitialized`) must call `super()._init`
+        from its `_init` method.
         """
         
         pass
+    
+    
+    # TODO: Add `reinitialize` method? That method would not necessarily
+    # be supported by a subclass, in which case according to the Python 3.9
+    # documentation for `NotImplementedError` the subclass should set the
+    # method to `None`.
