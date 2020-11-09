852c341fe45a9a6516b8663758a6655cefa6fa3e
harold <harold@harolds-mbp>
2020-11-09 15:30:52 -0500

    Edited script that shows recording audio files.

--- scripts/show_recording_audio_files.py
+++ scripts/show_recording_audio_files.py
@@ -9,6 +9,7 @@ Usage:
 
 
 from pathlib import Path
+import math
 import random
 import sys
 import wave
@@ -38,7 +39,7 @@ def get_recording_file_infos():
         for file_path in recording_dir_path.glob('**/*.wav'):
             
             try:
-                num_channels, sample_rate, length = \
+                channel_count, sample_rate, frame_count = \
                     get_wav_file_info(file_path)
                 
             except Exception as e:
@@ -49,28 +50,49 @@ def get_recording_file_infos():
                     f'{str(e)}.')
                 
             else:
-                info = (file_path, num_channels, sample_rate, length)
-                infos.append(info)
                 
+                duration = frame_count / sample_rate
+                
+                info = (
+                    file_path, channel_count, sample_rate, frame_count,
+                    duration)
+                
+                infos.append(info)
+    
+    infos.sort()
+    
     return infos
 
 
 def get_wav_file_info(file_path):
     
     with wave.open(str(file_path), 'rb') as file_:
-        num_channels = file_.getnchannels()
+        channel_count = file_.getnchannels()
         sample_rate = file_.getframerate()
-        length = file_.getnframes()
+        frame_count = file_.getnframes()
         
         if random.random() < _SIMULATED_ERROR_PROBABILITY:
             raise ValueError('Could not read wav file.')
     
-    return num_channels, sample_rate, length
+    return channel_count, sample_rate, frame_count
 
 
 def show_recording_file_infos(infos):
-    for file_path, num_channels, sample_rate, length in infos:
-        print(f'"{file_path}",{num_channels},{sample_rate},{length}')
+    
+    print(
+        'File Path,Channel Count,Sample Rate (Hz),Frame Count,'
+        'Duration')
+    
+    for file_path, channel_count, sample_rate, frame_count, duration in infos:
+        
+        hours = int(duration // 3600)
+        minutes = int((duration // 60) % 60)
+        seconds = int(math.floor(duration % 60))
+        tenths = int(round(10 * (duration % 1)))
+        
+        print(
+            f'"{file_path}",{channel_count},{sample_rate},{frame_count},'
+            f'{hours}:{minutes:02d}:{seconds:02d}.{tenths}')
            
     
 if __name__ == '__main__':
