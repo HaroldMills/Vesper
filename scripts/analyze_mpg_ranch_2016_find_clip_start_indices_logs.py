0d0db3029d8824366d3b2fc35d6c48d0348fdabb
harold <harold@harolds-mbp>
2020-11-24 20:48:39 -0500

    Added script that analyzes some MPG Ranch 2016 archive command logs.

---
+++ scripts/analyze_mpg_ranch_2016_find_clip_start_indices_logs.py
@@ -0,0 +1,246 @@
+"""
+Script that analyzes MPG Ranch 2016 archive logs of commands that add
+clip start indices.
+"""
+
+
+from collections import defaultdict
+from pathlib import Path
+import csv
+import re
+
+
+'''
+Input: job logs
+
+Output: File "MPG Ranch 2016 Add Clip Start Index Clip Counts.csv"
+
+Columns:
+    Station Channel
+    Start Time
+    Duration
+    Detector
+    Clips
+    Not Found
+    Short
+    All-Zero
+    Padded
+    
+Output: File "MPG Ranch 2016 Short Clip Counts.csv"
+
+Columns:
+    Clip Length
+    Clip Count
+'''
+
+
+ARCHIVE_DIR_PATH = Path('/Volumes/2012_2015_2016/2016_NFC/2016_NFC_All')
+
+LOG_DIR_PATH = ARCHIVE_DIR_PATH / 'Logs' / 'Jobs'
+
+LOG_FILE_NAME_FORMAT = 'Job {}.log'
+
+JOB_NUMS = (88, 100, 118, 122, 123, 125, 126)
+
+OUTPUT_FILE_PATH = \
+    ARCHIVE_DIR_PATH / 'MPG Ranch 2016 Add Clip Start Index Clip Counts.csv'
+
+CSV_COLUMN_NAMES = (
+    'Station Channel',
+    'Start Time',
+    'Duration',
+    'Detector',
+    'Clips',
+    'Not Found',
+    'Short',
+    'All-Zero',
+    'Zero-Padded',
+)
+
+SHORT_DETECTOR_NAMES = {
+    'Old Bird Thrush Detector': 'Thrush',
+    'Old Bird Tseep Detector': 'Tseep'
+}
+
+
+def main():
+    job_logs = [JobLog(n) for n in JOB_NUMS]
+    output_clip_counts(job_logs)
+
+
+CHANNEL_START_RE = re.compile(
+    r'^.* INFO     Processing (\d+) clips for recording channel "(.*) / '
+    r'.* / start (.*) / duration (.*) h / Channel (\d)" and detector '
+    r'"(.*)\.\.\.$')
+
+'''
+2020-11-21 21:52:20,845 INFO     Processing 1816 clips for recording channel "Ridge / SM2+ 010798 / start 2016-05-27 02:47:00+00:00 / duration 6.763 h / Channel 0" and detector "Old Bird Thrush Detector...
+2020-11-21 21:52:21,751 WARNING      Could not find samples of clip "Ridge / SMX-NFC RD Output / Old Bird Thrush Detector / start 2016-05-27 02:50:25+00:00 / duration 0.408 s" in recording channel.
+2020-11-21 21:52:52,794 INFO         For clip 4367378 at end of recording, found 462 of 13820 clip samples, including 0 trailing zeros.
+2020-11-21 22:49:15,427 WARNING      Found 25 copies of length-2 clip "Ridge / SMX-NFC RD Output / Old Bird Tseep Detector / start 2016-07-03 10:58:56+00:00 / duration 0.000 s".
+2020-11-22 04:38:19,370 WARNING      Encountered unexpected all-zero clip "Ridge / SMX-NFC RD Output / Old Bird Thrush Detector / start 2016-09-16 13:46:03+00:00 / duration 0.113 s". 
+'''
+
+
+class JobLog:
+    
+    
+    def __init__(self, job_num):
+        
+        print(f'Parsing log for job {job_num}...')
+        
+        self.job_num = job_num
+        
+        self._counts = []
+        self._channel_counts = None
+        
+        lines = read_job_log(job_num)
+        
+        for line in lines:
+            
+            if self._parse_channel_start_line(line):
+                pass
+            
+            elif self._parse_not_found_clip_line(line):
+                pass
+            
+            elif self._parse_short_clip_line(line):
+                pass
+            
+            elif self._parse_all_zero_clip_line(line):
+                pass
+            
+            elif self._parse_zero_padded_clip_line(line):
+                pass
+            
+            else:
+                self._check_for_warning_line(line)
+        
+        self._complete_channel_counts_if_needed()
+    
+    
+    @property
+    def counts(self):
+        return self._counts
+    
+    
+    def _parse_channel_start_line(self, line):
+        
+        m = CHANNEL_START_RE.match(line)
+        
+        if m is not None:
+            
+            (clip_count, station_name, start_time, duration, channel_num,
+             detector_name) = m.groups()
+             
+            clip_count = int(clip_count)
+             
+            # print(
+            #     f'    Channel start {clip_count}, {station_name}, '
+            #     f'{channel_num}, {start_time}, {duration}, '
+            #     f'{detector_name}...')
+            
+            self._complete_channel_counts_if_needed()
+
+            counts = defaultdict(int)
+            counts['Station Channel'] = f'{station_name} {channel_num}'
+            counts['Start Time'] = start_time
+            counts['Duration'] = duration
+            counts['Detector'] = SHORT_DETECTOR_NAMES[detector_name]
+            counts['Clips'] = clip_count
+            self._channel_counts = counts
+            
+            return True
+            
+        else:
+            return False
+    
+    
+    def _complete_channel_counts_if_needed(self):
+        if self._channel_counts is not None:
+            self._counts.append(self._channel_counts)
+    
+    
+    def _parse_not_found_clip_line(self, line):
+        
+        if line.find('WARNING      Could not find samples of clip') != -1:
+            # print('    Clip not found...')
+            self._channel_counts['Not Found'] += 1
+            return True
+        
+        else:
+            return False
+    
+    
+    def _parse_short_clip_line(self, line):
+        
+        if line.find(' copies of length-') != -1:
+            # print('    Short clip...')
+            self._channel_counts['Short'] += 1
+            self._channel_counts['Not Found'] += 1
+            return True
+        
+        else:
+            return False
+        
+    
+    def _parse_all_zero_clip_line(self, line):
+        
+        if line.find(' WARNING      Encountered unexpected all-zero clip ') \
+                != -1:
+            
+            # print('    Zero clip...')
+            self._channel_counts['All-Zero'] += 1
+            self._channel_counts['Not Found'] += 1
+            return True
+        
+        else:
+            return False
+        
+        
+    def _parse_zero_padded_clip_line(self, line):
+        
+        if line.find(' at end of recording, found ') != -1:
+            # print('    End of recording clip...')
+            self._channel_counts['Zero-Padded'] += 1
+            return True
+        
+        else:
+            return False
+        
+    
+    def _check_for_warning_line(self, line):
+        if line.find('WARNING') != -1:
+            print(f'    Unhandled WARNING line: {line}')
+        
+        
+def read_job_log(job_num):
+    log_file_path = get_log_file_path(job_num)
+    with open(log_file_path, 'r') as log_file:
+        return log_file.read().split('\n')
+
+
+def get_log_file_path(job_num):
+    log_file_name = LOG_FILE_NAME_FORMAT.format(job_num)
+    return LOG_DIR_PATH / log_file_name
+
+
+def output_clip_counts(job_logs):
+    
+    tuples = []
+    for log in job_logs:
+        for c in log.counts:
+            t = tuple(c[n] for n in CSV_COLUMN_NAMES)
+            tuples.append(t)
+    
+    tuples.sort()
+    
+    with open(OUTPUT_FILE_PATH, 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(CSV_COLUMN_NAMES)
+        for t in tuples:
+            writer.writerow(t)
+            
+
+if __name__ == '__main__':
+    main()
