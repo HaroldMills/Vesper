80ea0f2a430621c1e9e00d6f631c947f332b8917
Harold Mills <harold.mills@gmail.com>
2021-01-15 17:31:04 -0500

    Added script that compares two clip metadata CSV files.

---
+++ scripts/compare_clip_metadata_csv_files.py
@@ -0,0 +1,122 @@
+"""
+Script that compares two clip metadata CSV files.
+
+This script was written to check that the clip metadata CSV files output
+after the switch from PyEphem to Skyfield were almost equal to those output
+before. Solar event time columns are allowed to differ by up to one second,
+and the lunar altitude and illumination columns are allowed to differ by
+up to .1 degrees or percent.
+"""
+
+
+from pathlib import Path
+import csv
+import datetime
+import sys
+
+
+DIR_PATH = Path('/Users/harold/Desktop')
+FILE_NAME_A = 'old.csv'
+FILE_NAME_B = 'new.csv'
+
+FILE_PATH_A = DIR_PATH / FILE_NAME_A
+FILE_PATH_B = DIR_PATH / FILE_NAME_B
+
+
+def main():
+    
+    rows_a = read_csv_file(FILE_PATH_A)
+    rows_b = read_csv_file(FILE_PATH_B)
+    
+    compare_row_counts(rows_a, rows_b)
+    compare_headers(rows_a[0], rows_b[0])
+    compare_rows(rows_a[1:], rows_b[1:])
+    
+    print('Files are the same within the allowed differences.')
+
+
+def read_csv_file(file_path):
+    with open(file_path) as csv_file:
+        reader = csv.reader(csv_file)
+        return [row for row in reader]
+
+
+def compare_row_counts(rows_a, rows_b):
+    if len(rows_a) != len(rows_b):
+        print('CSV file row counts differ.')
+        print(f'File "{str(FILE_PATH_A)}" has {len(rows_a)} rows.')
+        print(f'File "{str(FILE_PATH_B)}" has {len(rows_b)} rows.')
+        sys.exit(1)
+
+
+def compare_headers(header_a, header_b):
+    if header_a != header_b:
+        print('CSV file headers differ.')
+        print(f'File "{str(FILE_PATH_A)}" header is {header_a}.')
+        print(f'File "{str(FILE_PATH_B)}" header is {header_b}.')
+        sys.exit(1)
+        
+        
+def compare_rows(rows_a, rows_b):
+    
+    for i, (row_a, row_b) in enumerate(zip(rows_a, rows_b)):
+        
+        row_num = i + 2
+        
+        compare_row_cell_counts(row_num, row_a, row_b)
+        
+        for j, (value_a, value_b) in enumerate(zip(row_a, row_b)):
+            col_num = j + 1
+            compare_row_cell_values(row_num, col_num, value_a, value_b)
+
+
+def compare_row_cell_counts(row_num, row_a, row_b):
+    if len(row_a) != len(row_b):
+        print(f'Cell counts differ at row {row_num}.')
+        print(f'File "{str(FILE_PATH_A)}" row has {len(row_a)} columns.')
+        print(f'File "{str(FILE_PATH_B)}" row has {len(row_b)} columns.')
+        print()
+
+
+def compare_row_cell_values(row_num, col_num, value_a, value_b):
+    
+    if col_num <= 13:
+        
+        if value_a != value_b:
+            print(f'Cell values differ at row {row_num}, column {col_num}.')
+            print(f'File "{str(FILE_PATH_A)}" value is {value_a}.')
+            print(f'File "{str(FILE_PATH_B)}" value is {value_b}.')
+            sys.exit(1)
+    
+    elif col_num <= 21:
+        
+        time_a = parse_datetime(value_a)
+        time_b = parse_datetime(value_b)
+        diff = abs((time_a - time_b).total_seconds())
+        
+        # We allow solar event times to differ by one second for the
+        # switch from PyEphem to Skyfield.
+        if diff >= 2:
+            print(
+                f'Solar event times differ at row {row_num}, '
+                f'column {col_num}.')
+            print(f'File "{str(FILE_PATH_A)}" value is {value_a}.')
+            print(f'File "{str(FILE_PATH_B)}" value is {value_b}.')
+    
+    else:
+        
+        diff = abs(float(value_a) - float(value_b))
+        
+        if diff >= .2:
+            print(f'Cell values differ at row {row_num}, column {col_num}.')
+            print(f'File "{str(FILE_PATH_A)}" value is {value_a}.')
+            print(f'File "{str(FILE_PATH_B)}" value is {value_b}.')
+            sys.exit(1)
+
+
+def parse_datetime(s):
+    return datetime.datetime.strptime(s, '%m/%d/%y %H:%M:%S')
+
+
+if __name__ == '__main__':
+    main()
