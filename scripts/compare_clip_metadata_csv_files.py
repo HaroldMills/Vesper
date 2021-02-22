f6997e89adb59a5edbab3853894db663fc9dabb8
Harold Mills <harold.mills@gmail.com>
2021-02-22 11:28:49 -0500

    Modified script that compares clip metadata CSV files.
    The script now counts unacceptable cell value differences rather than
    quitting on the first one.

--- scripts/compare_clip_metadata_csv_files.py
+++ scripts/compare_clip_metadata_csv_files.py
@@ -30,9 +30,13 @@ def main():
     
     compare_row_counts(rows_a, rows_b)
     compare_headers(rows_a[0], rows_b[0])
-    compare_rows(rows_a[1:], rows_b[1:])
     
-    print('Files are the same within the allowed differences.')
+    diff_count = compare_rows(rows_a[1:], rows_b[1:])
+    
+    if diff_count == 0:
+        print('Files are the same within the allowed differences.')
+    else:
+        print(f'Found {diff_count} differences outside of those allowed.')
 
 
 def read_csv_file(file_path):
@@ -44,20 +48,22 @@ def read_csv_file(file_path):
 def compare_row_counts(rows_a, rows_b):
     if len(rows_a) != len(rows_b):
         print('CSV file row counts differ.')
-        print(f'File "{str(FILE_PATH_A)}" has {len(rows_a)} rows.')
-        print(f'File "{str(FILE_PATH_B)}" has {len(rows_b)} rows.')
+        print(f'    File "{str(FILE_PATH_A)}" has {len(rows_a)} rows.')
+        print(f'    File "{str(FILE_PATH_B)}" has {len(rows_b)} rows.')
         sys.exit(1)
 
 
 def compare_headers(header_a, header_b):
     if header_a != header_b:
         print('CSV file headers differ.')
-        print(f'File "{str(FILE_PATH_A)}" header is {header_a}.')
-        print(f'File "{str(FILE_PATH_B)}" header is {header_b}.')
+        print(f'    File "{str(FILE_PATH_A)}" header is {header_a}.')
+        print(f'    File "{str(FILE_PATH_B)}" header is {header_b}.')
         sys.exit(1)
         
         
 def compare_rows(rows_a, rows_b):
+    
+    diff_count = 0
     
     for i, (row_a, row_b) in enumerate(zip(rows_a, rows_b)):
         
@@ -67,27 +73,30 @@ def compare_rows(rows_a, rows_b):
         
         for j, (value_a, value_b) in enumerate(zip(row_a, row_b)):
             col_num = j + 1
-            compare_row_cell_values(row_num, col_num, value_a, value_b)
+            if row_cell_values_differ(row_num, col_num, value_a, value_b):
+                diff_count += 1
+    
+    return diff_count
 
 
 def compare_row_cell_counts(row_num, row_a, row_b):
     if len(row_a) != len(row_b):
         print(f'Cell counts differ at row {row_num}.')
-        print(f'File "{str(FILE_PATH_A)}" row has {len(row_a)} columns.')
-        print(f'File "{str(FILE_PATH_B)}" row has {len(row_b)} columns.')
-        print()
+        print(f'    File "{str(FILE_PATH_A)}" row has {len(row_a)} columns.')
+        print(f'    File "{str(FILE_PATH_B)}" row has {len(row_b)} columns.')
+        sys.exit(1)
 
 
-def compare_row_cell_values(row_num, col_num, value_a, value_b):
+def row_cell_values_differ(row_num, col_num, value_a, value_b):
     
     if col_num <= 13:
         
         if value_a != value_b:
             print(f'Cell values differ at row {row_num}, column {col_num}.')
-            print(f'File "{str(FILE_PATH_A)}" value is {value_a}.')
-            print(f'File "{str(FILE_PATH_B)}" value is {value_b}.')
-            sys.exit(1)
-    
+            print(f'    File "{str(FILE_PATH_A)}" value is {value_a}.')
+            print(f'    File "{str(FILE_PATH_B)}" value is {value_b}.')
+            return True
+            
     elif col_num <= 21:
         
         time_a = parse_datetime(value_a)
@@ -100,8 +109,9 @@ def compare_row_cell_values(row_num, col_num, value_a, value_b):
             print(
                 f'Solar event times differ at row {row_num}, '
                 f'column {col_num}.')
-            print(f'File "{str(FILE_PATH_A)}" value is {value_a}.')
-            print(f'File "{str(FILE_PATH_B)}" value is {value_b}.')
+            print(f'    File "{str(FILE_PATH_A)}" value is {value_a}.')
+            print(f'    File "{str(FILE_PATH_B)}" value is {value_b}.')
+            return True
     
     else:
         
@@ -109,9 +119,11 @@ def compare_row_cell_values(row_num, col_num, value_a, value_b):
         
         if diff >= .2:
             print(f'Cell values differ at row {row_num}, column {col_num}.')
-            print(f'File "{str(FILE_PATH_A)}" value is {value_a}.')
-            print(f'File "{str(FILE_PATH_B)}" value is {value_b}.')
-            sys.exit(1)
+            print(f'    File "{str(FILE_PATH_A)}" value is {value_a}.')
+            print(f'    File "{str(FILE_PATH_B)}" value is {value_b}.')
+            return True
+    
+    return False
 
 
 def parse_datetime(s):
