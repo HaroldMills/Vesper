5ff98d71db4151936e6ad848a16c646ee0d8d3c2
Harold Mills <harold.mills@gmail.com>
2022-02-17 14:24:31 -0500

    Increment Vesper version to 0.4.11a2.

--- vesper/version.py
+++ vesper/version.py
@@ -9,7 +9,7 @@ module that needs the Vesper version should obtain it from this module.
 major_number = 0
 minor_number = 4
 patch_number = 11
-suffix = 'a1'
+suffix = 'a2'
 
 major_version = f'{major_number}'
 minor_version = f'{major_version}.{minor_number}'
