290a817740eb7f69abd8ef4ecee80d632819ef74
Harold Mills <harold.mills@gmail.com>
2022-02-24 17:20:03 -0500

    Require tag clips command tag argument.

--- vesper/django/app/tag_clips_form.py
+++ vesper/django/app/tag_clips_form.py
@@ -7,3 +7,7 @@ class TagClipsForm(ClipSetForm):
     
     clip_count = forms.IntegerField(
         label='Clip count', min_value=0, required=False)
+
+    def __init__(self, *args, **kwargs):
+        kwargs['clip_set_tag_required'] = True
+        super().__init__(*args, **kwargs)
