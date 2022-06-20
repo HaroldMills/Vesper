8c0bcf4e75e2afbb2004adfc828214b3bc367b61
Harold Mills <harold.mills@gmail.com>
2022-06-20 16:42:33 -0400

    Delete dead code from audio file utils.

--- vesper/signal/audio_file_utils.py
+++ vesper/signal/audio_file_utils.py
@@ -3,8 +3,6 @@
 
 import os.path
 
-# from vesper.signal.array_signal import ArraySignal
-# from vesper.signal.multichannel_array_signal import MultichannelArraySignal
 from vesper.signal.time_axis import TimeAxis
 from vesper.signal.unsupported_audio_file_error import UnsupportedAudioFileError
 from vesper.signal.wave_audio_file import WaveAudioFileType
@@ -28,82 +26,3 @@ def get_audio_file_type(file_path):
         
     # If we get here, the file was not recognized by any audio file type.
     return None
-    
-    
-# def create_multichannel_array_signal(
-#         file_, file_type=None, reference_datetime=None):
-#
-#     """Creates a `MultichannelArraySignal` from an audio file."""
-#
-#     file_type = _get_file_type(file_, file_type)
-#
-#     with file_type.reader_class(file_) as reader:
-#
-#         samples = reader.read()
-#
-#         time_axis = TimeAxis(
-#             length=samples.shape[1],
-#             sample_rate=reader.sample_rate,
-#             reference_datetime=reference_datetime)
-#
-#         return MultichannelArraySignal(time_axis=time_axis, samples=samples)
-#
-#
-# def _get_file_type(file_, file_type):
-#
-#     if isinstance(file_, str):
-#
-#         if file_type is None:
-#             file_type = _infer_file_type(file_)
-#
-#         return file_type
-#
-#     else:
-#         # `file_` is a file-like object
-#
-#         if file_type is None:
-#             raise ValueError('File type not specified for file-like object.')
-#
-#         else:
-#             return file_type
-#
-#
-# def _infer_file_type(file_path):
-#
-#     file_type = get_audio_file_type(file_path)
-#
-#     if file_type is None:
-#         raise UnsupportedAudioFileError(
-#             'Could not infer audio file type for file "{}".'.format(file_path))
-#
-#     else:
-#         return file_type
-#
-#
-# def create_array_signal(file_, file_type=None, reference_datetime=None):
-#
-#     """Creates an `ArraySignal` from a single-channel audio file."""
-#
-#     file_type = _get_file_type(file_, file_type)
-#
-#     with file_type.reader_class(file_) as reader:
-#
-#         if reader.num_channels > 1:
-#
-#             if isinstance(file_, str):
-#                 file_path = ' "{}"'.format(file_)
-#             else:
-#                 file_path = ''
-#
-#             raise ValueError((
-#                 'Cannot create array signal from multichannel file{}. '
-#                 'File must have only one channel.').format(file_path))
-#
-#         samples = reader.read()[0]
-#
-#         time_axis = TimeAxis(
-#             length = len(samples),
-#             sample_rate=reader.sample_rate,
-#             reference_datetime=reference_datetime)
-#
-#         return ArraySignal(time_axis=time_axis, samples=samples)
--- vesper/signal/tests/test_audio_file_utils.py
+++ vesper/signal/tests/test_audio_file_utils.py
@@ -9,7 +9,7 @@ import vesper.signal.audio_file_utils as audio_file_utils
 import vesper.signal.tests.utils as utils
 
 
-class WaveAudioFileTests(TestCase):
+class AudioFileUtilsTests(TestCase):
 
 
     def test_get_audio_file_type(self):
@@ -34,78 +34,3 @@ class WaveAudioFileTests(TestCase):
     def _assert_function_raises(self, cls, function, file_name):
         file_path = utils.create_test_audio_file_path(file_name)
         self._assert_raises(cls, function, file_path)
-        
-        
-    # def test_create_multichannel_array_signal(self):
-    #
-    #     cases = [
-    #         ('One Channel.wav', 1, 100, 22050, np.int16),
-    #         ('Two Channels.wav', 2, 10, 24000, np.int16),
-    #         ('Four Channels.wav', 4, 100, 22050, np.int16)
-    #     ]
-    #
-    #     for file_name, num_channels, length, sample_rate, dtype in cases:
-    #
-    #         file_path = utils.create_test_audio_file_path(file_name)
-    #
-    #         audio = audio_file_utils.create_multichannel_array_signal(file_path)
-    #
-    #         self.assertIsInstance(audio, MultichannelArraySignal)
-    #         self.assertEqual(len(audio), num_channels)
-    #         self.assertEqual(audio.time_axis.length, length)
-    #         self.assertEqual(audio.time_axis.sample_rate, sample_rate)
-    #         self.assertEqual(audio.dtype, dtype)
-    #
-    #         expected = utils.create_samples(
-    #             (num_channels, length), factor=1000, dtype=dtype)
-    #         utils.assert_arrays_equal(audio[:], expected, strict=True)
-    #
-    #
-    # def test_create_multichannel_array_signal_errors(self):
-    #
-    #     function = audio_file_utils.create_multichannel_array_signal
-    #
-    #     # Nonexistent file.
-    #     self._assert_function_raises(ValueError, function, 'Nonexistent')
-    #
-    #     # Unsupported file.
-    #     self._assert_function_raises(
-    #         UnsupportedAudioFileError, function, 'Empty')
-    #
-    #
-    # def test_create_array_signal(self):
-    #
-    #     cases = [
-    #         ('One Channel.wav', 100, 22050, np.int16),
-    #     ]
-    #
-    #     for file_name, length, sample_rate, dtype in cases:
-    #
-    #         file_path = utils.create_test_audio_file_path(file_name)
-    #
-    #         audio = audio_file_utils.create_array_signal(file_path)
-    #
-    #         self.assertIsInstance(audio, ArraySignal)
-    #         self.assertEqual(len(audio), length)
-    #         self.assertEqual(audio.time_axis.sample_rate, sample_rate)
-    #         self.assertEqual(audio.dtype, dtype)
-    #
-    #         expected = utils.create_samples((length,), factor=1000, dtype=dtype)
-    #         utils.assert_arrays_equal(audio[:], expected, strict=True)
-    #
-    #
-    # def test_create_array_signal_errors(self):
-    #
-    #     function = audio_file_utils.create_array_signal
-    #
-    #     # Nonexistent file.
-    #     self._assert_function_raises(ValueError, function, 'Nonexistent')
-    #
-    #     # Unsupported file.
-    #     self._assert_function_raises(
-    #         UnsupportedAudioFileError, function, 'Empty')
-    #
-    #     # Multichannel file.
-    #     self._assert_function_raises(ValueError, function, 'Two Channels.wav')
-        
-        
