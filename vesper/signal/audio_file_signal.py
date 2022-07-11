2cabe106d002d7ec2beef9b8e2b255c7de2ad086
Harold Mills <harold.mills@gmail.com>
2022-07-11 13:58:21 -0400

    Remove outdated comment.

--- vesper/signal/audio_file_signal.py
+++ vesper/signal/audio_file_signal.py
@@ -2,25 +2,6 @@ from vesper.signal.signal import Signal
 from vesper.signal.time_axis import TimeAxis
 
 
-'''
-Design of Vesper audio file signals:
-
-* Support various audio file formats through plugins.
-
-* WaveFileSignal plugin supports WAVE files via the Python `wave` module.
-
-* AiffFileSignal plugin supports AIFF files via the Python `aifc` module.
-
-* Optional SoundFileSignal plugin supports various kinds of audio files
-  via the SoundFile package. I would like for this plugin to be optional
-  in the sense that if you won't use it you don't have to install the
-  SoundFile package and its dependencies.
-
-* Audio file signal class initializer accepts a file path or a file-like
-  object. Raises an `UnsupportedAudioFileError` if the file is not supported.
-'''
-
-
 class AudioFileSignal(Signal):
     
     
