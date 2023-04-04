3bd650961b5193ab588591055033beac45f6f606
Harold Mills <harold.mills@gmail.com>
2023-04-04 11:44:05 -0400

    Improve Django create-clips view.

--- vesper/django/old_bird/views.py
+++ vesper/django/old_bird/views.py
@@ -3,7 +3,7 @@ import logging
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
-from django.http import HttpResponse, HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 from django.views import View
 
 from vesper.django.app.models import (
@@ -47,14 +47,21 @@ class CreateLrgvClipsView(LoginRequiredMixin, View):
 
         try:
             with transaction.atomic():
+                clips = []
                 for clip_info in content['clips']:
-                    self._create_clip(clip_info)
-
+                    clip_id, recording_id = self._create_clip(clip_info)
+                    clips.append({
+                        'clip_id': clip_id,
+                        'recording_id': recording_id
+                    })
+                    
         except Exception as e:
             error_message = _get_error_message(e, clip_info)
-            return HttpResponseBadRequest(reason=error_message)
+            return HttpResponseBadRequest(
+                content=error_message, content_type='text/plain')
         
-        return HttpResponse('Ok')
+        data = {'clips': clips}
+        return JsonResponse(data)
         
 
     def _create_clip(self, clip_info):
@@ -63,7 +70,7 @@ class CreateLrgvClipsView(LoginRequiredMixin, View):
         station, mic_output = self._get_station_mic_output_pair(station_name)
 
         start_time = clip_info['start_time']
-        start_time = _parse_datetime(start_time, station)
+        start_time = _parse_datetime(start_time, station, 'clip start')
 
         length = clip_info['length']
 
@@ -71,8 +78,9 @@ class CreateLrgvClipsView(LoginRequiredMixin, View):
         creating_processor = self._get_detector(detector_name)
 
         recording_info = clip_info.get('recording')
-        recording_channel, sample_rate, end_time = self._get_recording_channel(
-            station, mic_output, start_time, length, recording_info)
+        recording_channel, sample_rate, end_time, new_recording_id = \
+            self._get_recording_channel(
+                station, mic_output, start_time, length, recording_info)
 
         # We set the start index to `None` since we do not know it
         # exactly. We will attempt to correct this later by locating
@@ -83,7 +91,7 @@ class CreateLrgvClipsView(LoginRequiredMixin, View):
 
         creation_time = time_utils.get_utc_now()
 
-        Clip.objects.create(
+        clip = Clip.objects.create(
             station=station,
             mic_output=mic_output,
             recording_channel=recording_channel,
@@ -98,6 +106,8 @@ class CreateLrgvClipsView(LoginRequiredMixin, View):
             creating_job=None,
             creating_processor=creating_processor
         )
+
+        return clip.id, new_recording_id
 
 
     def _get_station_mic_output_pair(self, station_name):
@@ -121,6 +131,9 @@ class CreateLrgvClipsView(LoginRequiredMixin, View):
             self, station, mic_output, clip_start_time, clip_length,
             recording_info):
         
+        def localize(dt):
+            return _localize(dt, station)
+        
         # We use a placeholder clip end time to try to locate the
         # clip's recording. We don't know the real clip end time yet
         # because while we have the clip's length we don't have its
@@ -142,11 +155,13 @@ class CreateLrgvClipsView(LoginRequiredMixin, View):
                 raise ValueError(
                     f'Could not find existing recording for station '
                     f'"{station.name}" for clip with start time '
-                    f'{clip_start_time}, and no recording information '
-                    f'was provided from which to create one.')
+                    f'{localize(clip_start_time)}, and no recording '
+                    f'information was provided from which to create one.')
 
             recording = \
                 self._create_recording(station, mic_output, recording_info)
+            
+            new_recording_id = recording.id
         
         else:
             # found at least one recording
@@ -155,15 +170,16 @@ class CreateLrgvClipsView(LoginRequiredMixin, View):
                 raise ValueError(
                     f'Found more than one recording for station '
                     f'"{station.name}" for clip with start time '
-                    f'{clip_start_time}.')
+                    f'{localize(clip_start_time)}.')
             
             else:
                 # found exactly one recording
             
                 recording = recordings[0]
+                new_recording_id = None
 
                 if recording_info is not None:
-                    _check_recording(recording, recording_info)
+                    _check_recording(recording, recording_info, station)
 
         channels = recording.channels.all()
         channel_count = channels.count()
@@ -171,15 +187,14 @@ class CreateLrgvClipsView(LoginRequiredMixin, View):
         if channel_count > 1:
             raise ValueError(
                 f'Recording for station "{station.name}" that includes '
-                f'clip start time {clip_start_time} has '
+                f'clip start time {localize(clip_start_time)} has '
                 f'{recording.num_channels} channels instead of just one.')
         
         # Check that clip does not start before recording.
         if clip_start_time < recording.start_time:
             raise ValueError(
-                f'Clip start time {clip_start_time} precedes '
-                f'start time {recording.start_time} of only '
-                f'intersecting recording.')
+                f'Clip start time {localize(clip_start_time)} precedes '
+                f'recording start time {localize(recording.start_time)}.')
         
         # Get sample rate and real clip end time.
         sample_rate = recording.sample_rate
@@ -189,17 +204,16 @@ class CreateLrgvClipsView(LoginRequiredMixin, View):
         # Check that clip does not end after recording.
         if clip_end_time > recording.end_time:
             raise ValueError(
-                f'Clip end time {clip_end_time} follows '
-                f'end time {recording.end_time} of only '
-                f'intersecting recording.')
+                f'Clip end time {localize(clip_end_time)} follows '
+                f'recording end time {localize(recording.end_time)}.')
                 
-        # If we get here, the existing recording has one channel.
-        return channels.first(), sample_rate, clip_end_time
+        return channels.first(), sample_rate, clip_end_time, new_recording_id
 
 
     def _create_recording(self, station, mic_output, recording_info):
 
-        start_time = _parse_datetime(recording_info['start_time'], station)
+        start_time = _parse_datetime(
+            recording_info['start_time'], station, 'recording start')
         length = recording_info['length']
         sample_rate = recording_info['sample_rate']
 
@@ -262,8 +276,8 @@ def _get_error_message(exception, clip_info):
 
     return (
         f'Could not create clip for station "{station_name}", start '
-        f'time {start_time}, and detector "{detector_name}". Error '
-        f'message was: {exception}. No clips or recordings will be '
+        f'time "{start_time}", and detector "{detector_name}". Error '
+        f'message was: {exception} No clips or recordings will be '
         f'created for this request.')
 
 
@@ -273,7 +287,7 @@ _DATE_TIME_FORMATS = (
 )
 
 
-def _parse_datetime(text, station):
+def _parse_datetime(text, station, name):
 
     for format in _DATE_TIME_FORMATS:
 
@@ -287,19 +301,27 @@ def _parse_datetime(text, station):
         return station.local_to_utc(local_dt)
     
     # If we get here, none of the parse attempts succeeded.
-    raise ValueError(f'Could not parse date and time "{text}".')
+    raise ValueError(f'Could not parse {name} time "{text}".')
 
 
-def _check_recording(recording, recording_info):
+def _localize(dt, station):
+    dt = station.utc_to_local(dt)
+    return dt.replace(tzinfo=None)
 
-    start_time = \
-        _parse_datetime(recording_info['start_time'], recording.station)
+
+def _check_recording(recording, recording_info, station):
+
+    def localize(dt):
+        return _localize(dt, station)
+
+    start_time = _parse_datetime(
+        recording_info['start_time'], recording.station, 'recording start')
 
     if start_time != recording.start_time:
         raise ValueError(
-            f'Specified recording start time {start_time} does not match '
-            f'start time {recording.start_time} of recording already in '
-            f'archive.')
+            f'Specified recording start time {localize(start_time)} '
+            f'does not match start time {localize(recording.start_time)} '
+            f'of recording already in archive.')
     
     length = recording_info['length']
 
