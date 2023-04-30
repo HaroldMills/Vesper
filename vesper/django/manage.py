5004d2d302054e1b100c361d5ed578d37411b53e
Harold Mills <harold.mills@gmail.com>
2023-04-30 09:50:37 -0400

    Use more generic manage.py.

--- vesper/django/manage.py
+++ vesper/django/manage.py
@@ -1,42 +1,13 @@
 #!/usr/bin/env python
-
-# Vesper server administration script.
-# 
-# This script is derived from the standard Django manage.py script.
-
-
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-# from vesper.archive_paths import archive_paths
-
 
 def main():
-    
-    args = sys.argv
-    
-    # We did not used to do this, but when we switched to version 3 of
-    # the `conda-build` package the command `vesper_admin runserver`
-    # began failing on Windows (at least, and also on macOS, if I
-    # remember correctly) with an error message indicating that
-    # `python.exe` could not find `vesper_admin` in its environment's
-    # `Scripts` directory (indeed, there is no such file there). The
-    # code below is a workaround for that problem. Interestingly,
-    # the command `vesper_admin` (with no arguments) continued to work
-    # as before.
-    #
-    # In the future, we might just want to do away with the
-    # `vesper_admin` entry point, which is just a shorthand for
-    # `python -m vesper.django.manage`.
-    if args[0].endswith('vesper_admin'):
-        args[0] = __file__
-        
-    # if 'createsuperuser' in args or 'runserver' in args:
-    #     _check_archive_dir()
-    
+    """Run administrative tasks."""
     os.environ.setdefault(
         'DJANGO_SETTINGS_MODULE', 'vesper.django.project.settings')
-
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -45,99 +16,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    
-    execute_from_command_line(args)
-    
-    
-# def _check_archive_dir():
-    
-#     """
-#     Checks that the purported archive directory appears to contain a
-#     Vesper archive.
-#     """
-    
-#     _check_database()
-#     _check_preferences()
-#     _check_presets()
-    
-    
-# def _check_database():
-
-#     from django.conf import settings
-    
-#     # See `vesper.util.archive_lock` module for `is_database_sqlite`.
-#     if is_database_sqlite():
-        
-#         file_path = archive_paths.sqlite_database_file_path
-        
-#         if not file_path.exists():
-            
-#             archive_dir_path = archive_paths.archive_dir_path
-#             relative_file_path = file_path.relative_to(archive_dir_path)
-        
-#             print(
-#                 f'The directory "{archive_dir_path}" does not appear to '
-#                 f'be a Vesper archive directory, since it does not contain '
-#                 f'an archive database file "{relative_file_path}". Please '
-#                 f'run your command again in an archive directory.')
-            
-#             sys.exit(1)
-                        
-            
-# def _check_preferences():
-    
-#     file_path = archive_paths.preference_file_path
-    
-#     if not file_path.exists():
-        
-#         archive_dir_path = archive_paths.archive_dir_path
-#         relative_file_path = file_path.relative_to(archive_dir_path)
-        
-#         print(
-#             f'WARNING: The Vesper archive at "{archive_dir_path}" does '
-#             f'not contain a preference file "{relative_file_path.name}". '
-#             f'The server will use default preferences for this archive.')
-
-
-# def _check_presets():
-    
-#     dir_path = archive_paths.preset_dir_path
-    
-#     if not dir_path.exists():
-        
-#         archive_dir_path = archive_paths.archive_dir_path
-#         relative_dir_path = dir_path.relative_to(archive_dir_path)
-        
-#         print(
-#             f'WARNING: The Vesper archive at "{archive_dir_path}" does '
-#             f'not contain a preset directory "{relative_dir_path}". '
-#             f'No presets will be available for use with this archive.')
+    execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
     main()
-
-
-# The following is the standard Django manage.py script, as of Django 4.1.6:
-# #!/usr/bin/env python
-# """Django's command-line utility for administrative tasks."""
-# import os
-# import sys
-# 
-# 
-# def main():
-#     """Run administrative tasks."""
-#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booboo.settings')
-#     try:
-#         from django.core.management import execute_from_command_line
-#     except ImportError as exc:
-#         raise ImportError(
-#             "Couldn't import Django. Are you sure it's installed and "
-#             "available on your PYTHONPATH environment variable? Did you "
-#             "forget to activate a virtual environment?"
-#         ) from exc
-#     execute_from_command_line(sys.argv)
-# 
-# 
-# if __name__ == '__main__':
-#     main()
