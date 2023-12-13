7c473221629a035dfc8b235ac723602d88d3086b
Harold Mills <harold.mills@gmail.com>
2023-12-13 16:52:28 -0500

    Update recorder script docstring.

--- vesper/scripts/vesper_recorder.py
+++ vesper/scripts/vesper_recorder.py
@@ -3,11 +3,11 @@ Script version of the Vesper Recorder.
 
 The script takes no arguments.
 
-The Vesper Recorder requires a home directory whose location is specified
-by the `VESPER_RECORDER_HOME` environment variable. The home directory
-must contain a YAML configuration file named `Vesper Recorder Config.yaml`.
-An example configuration file including extensive comments is distributed
-with the recorder.
+The Vesper Recorder uses a *home directory* whose location is the
+current working directory, i.e. the directory in which the recorder
+was started. The home directory must contain a YAML configuration file
+named `Vesper Recorder Config.yaml`. An example configuration file
+including extensive comments is distributed with the recorder.
 
 The recorder logs messages to the file `Vesper Recorder Log.txt`, also in
 the recorder home directory. This script also logs essentially the same
@@ -21,13 +21,8 @@ issue the following commands from your Vesper Git repo folder:
     conda install -c conda-forge python-sounddevice
     pip install -e .
     
-To run the Vesper Recorder, first set the VESPER_RECORDER_HOME environment
-variables. On Unix, for example:
-
-    export VESPER_RECORDER_HOME="/Users/Harold/Desktop/Vesper Recorder"
-    
-Then, in an Anaconda Prompt or terminal window, run the Vesper Recorder
-script:
+To run the Vesper Recorder, run the following commands in an Anaconda
+Prompt or terminal window:
 
     conda activate vesper-recorder-dev
     vesper_recorder
