f05de3c7f04fabf5310ceab378e99bb214333737
Harold Mills <harold.mills@gmail.com>
2024-07-08 21:34:22 -0400

    Add S3 file upload retries to recorder.

--- vesper/recorder/s3_file_uploader.py
+++ vesper/recorder/s3_file_uploader.py
@@ -2,25 +2,25 @@
 An S3 file uploader takes a sequence of file paths as input and produces
 no output.
 
-For each input file, the uploader queues an asynchronous upload task.
-If the upload fails, the task adds the file to a *retry queue*. A
-*retry thread* periodically checks the queue and attempts to upload
-the queued files. It sleeps after either uploading all of the queued
-files or on the first failure. It should probably move a file for which
-uploading fails to the back of the queue to avoid starving files queued
-behind it. The basic idea here is to make a reasonable effort to recover
-from brief network outages.
+For each input file, the uploader queues an *upload task* to run in an
+async event loop on a *task runner* thread. The task re-enqueues itself
+on failure. The task runner checks the queue once per second normally,
+but only once per minute when there are failed tasks on it. The main
+aim of the failure handling is to weather network outages gracefully.
 '''
 
 
+from queue import Queue, Empty
+from threading import Thread
+import asyncio
 import logging
+import time
 
 import aioboto3
 
 from vesper.recorder.processor import Processor
 from vesper.recorder.status_table import StatusTable
 from vesper.util.bunch import Bunch
-import vesper.recorder.async_task_thread as async_task_thread
 
 
 _DEFAULT_AWS_PROFILE_NAME = 'default'
@@ -74,7 +74,7 @@ class S3FileUploader(Processor):
             dir_path, file_path, s.aws_profile_name, s.s3_bucket_name,
             s.s3_object_key_prefix, s.delete_uploaded_files)
         
-        async_task_thread.instance.submit(task)
+        _task_runner.enqueue_task(task)
 
 
     def get_status_tables(self):
@@ -105,6 +105,7 @@ class _UploadTask:
         self._s3_bucket_name = s3_bucket_name
         self._s3_object_key_prefix = s3_object_key_prefix
         self._delete_uploaded_file = delete_uploaded_file
+        self._failure_count = 0
 
 
     async def run(self):
@@ -117,9 +118,15 @@ class _UploadTask:
         # Get absolute audio file path.
         abs_file_path = self._dir_path / self._file_path
 
+        if self._failure_count == 0:
+            attempt_text = ''
+        else:
+            attempt_text = f' (attempt {self._failure_count + 1})'
+
         _logger.info(
             f'Uploading audio file "{abs_file_path}" to S3 bucket '
-            f'"{self._s3_bucket_name}", object key "{object_key}"...')
+            f'"{self._s3_bucket_name}", object key "{object_key}"'
+            f'{attempt_text}...')
         
         try:
 
@@ -134,17 +141,21 @@ class _UploadTask:
 
             _logger.warning(
                 f'Failed to upload file "{abs_file_path}" to S3 bucket '
-                f'"{self._s3_bucket_name}", object key "{object_key}". '
-                f'Exception message was: {e}')
+                f'"{self._s3_bucket_name}", object key "{object_key}"'
+                f'{attempt_text}. Exception message was: {e}')
             
-            # TODO: Retry failed uploads.
+            self._failure_count += 1
+            
+            return False
 
         else:
             # upload succeeded
-       
+
             if self._delete_uploaded_file:
                 self._delete_file(abs_file_path)
                 self._delete_empty_ancestor_dirs()
+
+            return True
 
 
     def _delete_file(self, file_path):
@@ -215,3 +226,74 @@ class _UploadTask:
                 # We can stop here, since any further directories will
                 # be ancestors of this one and hence not empty either.
                 break
+
+
+class _S3FileUploaderTaskRunner(Thread):
+
+
+    """
+    S3 file uploader task runner.
+
+    This thread runs asynchronous tasks that upload files to S3. It
+    receives the tasks via a thread-safe FIFO queue. Each task attempts
+    to upload one file. If the upload fails, the thread re-enqueues the
+    task and sleeps for awhile. It also sleeps when the queue is empty.
+    """
+
+
+    # TODO: Arrange for orderly shutdown of this thread. That probably
+    # means it shouldn't be a daemon thread. Perhaps there could be an
+    # `Event` that it checks in its run loop to see if it should quit,
+    # and the `Event` gets set during shutdown. The recorder wouldn't
+    # know specifically about the `Event`, but it would notify each
+    # processor class (or processor?) of shutdown, and the
+    # `S3FileUploader` class would respond by setting the event.
+
+
+    def __init__(self):
+        super().__init__(daemon=True)
+        self._normal_mode_sleep_period = 1        # seconds
+        self._failure_mode_sleep_period = 60      # seconds
+        self._tasks = Queue()
+
+
+    def enqueue_task(self, task):
+        self._tasks.put(task)
+
+
+    def run(self):
+        asyncio.run(self._run())
+
+
+    async def _run(self):
+
+        while True:
+
+            try:
+                task = self._tasks.get()
+
+            except Empty:
+                # no tasks to run
+
+                # Sleep before checking queue again.
+                time.sleep(self._normal_mode_sleep_period)
+
+            else:
+                # got a task to run
+
+                # Run task.
+                upload_succeeded = await task.run()
+
+                if not upload_succeeded:
+                    # upload failed
+
+                    # Re-enqueue failed task to retry later.
+                    self.enqueue_task(task)
+
+                    # Sleep before checking queue again.
+                    time.sleep(self._failure_mode_sleep_period)
+
+
+# The one and only `_S3FileUploaderTaskRunner` of the Vesper Recorder.
+_task_runner = _S3FileUploaderTaskRunner()
+_task_runner.start()
